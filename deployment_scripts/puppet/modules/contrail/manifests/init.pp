c0c4887e900baf14feba029ea179cf1a3ac2dfa3
Vyacheslav Struk <vstruk@mirantis.com>
2015-02-11 17:59:17 +0200

    Base network configuration and package installation
    Base network configuration for compute and contrail controller and
    package installation was implemented
    
    Co-Authored-By: Oleksandr Martsyniuk (omartsyniuk@mirantis.com)
    Change-Id: I417a0e3edd35ed6e2273fa558fd94a68c9f17a3f

--- deployment_scripts/puppet/modules/contrail/manifests/init.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/init.pp
@@ -1,36 +1,25 @@
 class contrail {
 
-  $settings = hiera('contrail')
-  $role = hiera('role')
-  $node_name = hiera('user_node_name')
+# General configuration
+$settings = hiera('contrail')
 
-  case $role {
-    /^*.controller$/: {
-      class { contrail::packages:
-        install => ['python-paramiko, contrail-fabric-utils, contrail-setup'],
-      }
-    }
-    /^compute$/: {
-      class { contrail::packages:
-        install => 'contrail-openstack-vrouter',
-        remove  => ['openvswitch-common','openvswitch-datapath-lts-saucy-dkms','openvswitch-switch',
-                  'nova-network','nova-api'],
-      }
-    }
-    /^base-os$/: {
-      case $node_name {
-        /^contrail.\d+$/: {
-          class { contrail::packages:
-            install => ['python-crypto','python-netaddr','python-paramiko',
-            'contrail-fabric-utils','contrail-setup'],
-            responsefile => 'contrail.preseed',
-          }
-        }
-        default: {
-          notify { "This is not a contrail controller node. Plugin tasks aborted": }
-        }
-      }
-    }
-  } 
-  
-} 
+# TODO
+#$plugin_version = $settings['metadata']['plugin_version']
+$plugin_version = '1.0.0'
+
+$network_scheme = hiera('network_scheme')
+$uid = hiera('uid')
+$master_ip = hiera('master_ip')
+$node_role = hiera('role')
+
+# Network configuration
+prepare_network_config($network_scheme)
+$ifname = get_private_ifname()
+$range_first = $settings['contrail_private_start']
+$range_last = $settings['contrail_private_end']
+$cidr = $settings['contrail_private_cidr'] # "cidr" is actually a network address with subnet, i.e. 192.168.150.0/24
+$netmask=cidr_to_netmask($cidr) # returns i.e. "255.255.255.0"
+$netmask_short=netmask_to_cidr($netmask) # returns i.e. "/24"
+$address=get_ip_from_range($range_first,$range_last,$netmask,$uid)
+
+}
