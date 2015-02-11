84ea1e2d038eef3955c2413ca794c09b0e3610fc
Vyacheslav Struk <vstruk@mirantis.com>
2015-02-11 17:59:17 +0200

    Base network configuration and package installation
    Base network configuration for compute and contrail controller and
    package installation was implemented
    
    Co-Authored-By: Oleksandr Martsyniuk (omartsyniuk@mirantis.com)
    Change-Id: I417a0e3edd35ed6e2273fa558fd94a68c9f17a3f

--- deployment_scripts/puppet/manifests/site-contrail-pre.pp
+++ deployment_scripts/puppet/manifests/site-contrail-pre.pp
@@ -1,5 +1,22 @@
+include contrail
+
+class { 'contrail::network':
+  node_role   => 'base-os',
+  address     => $contrail::address,
+  ifname      => $contrail::ifname,
+  netmask     => $contrail::netmask_short,
+} ->
+
+class { contrail::ssh:
+  password_auth => 'yes',
+} ->
+
 class { contrail::packages:
-  install => ['python-crypto','python-netaddr','python-paramiko',
-  'contrail-fabric-utils','contrail-setup'],
-  responsefile => 'contrail.preseed',
+  plugin_version => $contrail::plugin_version,
+  master_ip      => $contrail::master_ip,
+  master_port    => $contrail::master_port,
+  install        => ['python-crypto','python-netaddr','python-paramiko',
+                    'contrail-fabric-utils','contrail-setup'],
+  pip_install    => ['ecdsa-0.10','Fabric-1.7.0'],
+  responsefile   => 'contrail.preseed',
 }
--- deployment_scripts/puppet/modules/contrail/manifests/packages.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/packages.pp
@@ -2,12 +2,13 @@
 #   puppetlabs-apt
 #   puppetlabs-stdlib
 class contrail::packages (
-  $master_ip = hiera('master_ip'),
+  $master_ip,
   $master_port = '8080',
-  $plugin_version = $contrail::settings['metadata']['plugin_version'],
-  $install = undef,
+  $plugin_version,
+  $install,
   $remove = undef,
   $responsefile = undef,
+  $pip_install = undef,
   ) {
 
   class { 'apt': disable_keys => true }
@@ -27,6 +28,13 @@ class contrail::packages (
     packages     => '*',
   }
 
+  define exec_pip ( $path ){
+    exec { "Install-pip-package-${name}":
+      path     => '/usr/local/bin/:/usr/bin:/bin',
+      command  => "pip install --upgrade --no-deps --index-url='' ${path}/${name}.tar.gz",
+    }
+  }
+  
   if ($responsefile) {
     file { "/var/cache/debconf/${responsefile}" :
       ensure => file,
@@ -46,6 +54,12 @@ class contrail::packages (
     package { $remove:
       ensure     => absent,
       require    => Apt::Source['contrail-from-fuel-master'],
+    }
+  }
+  
+  if ($pip_install) {
+    exec_pip { $pip_install:
+      path => '/opt/contrail/python_packages'
     }
   }
 
