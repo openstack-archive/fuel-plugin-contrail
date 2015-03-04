fa6b2a9f7dc80f4e30790b85a88c2cf697e5fcb8
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-03-04 15:32:45 +0200

    Contrail controller nodes deployment stage 1
    This commit contains puppet classes which create testbed file
    for fabric tool and execute fab tasks on controller nodes.
    
    Change-Id: Ie434d327ee53a5564adde001785b54a50eb30ebc
    Co-Authored-By: Vyacheslav Struk (vstruk@mirantis.com)

---
+++ deployment_scripts/puppet/modules/contrail/manifests/setup.pp
@@ -0,0 +1,41 @@
+class contrail::setup (
+  $node_name
+  ) {
+
+  define run_fabric (
+    $hostgroup = undef,
+    $taskname = $name,
+    $command = undef,
+  ) {
+    Exec {
+      cwd => '/opt/contrail/utils',
+      path => '/bin:/usr/bin:/usr/local/bin',
+      logoutput => 'on_failure',
+    }
+    case $hostgroup {
+      control: {
+        exec { "Run-fabric-command-on-${hostgroup}":
+          command => "fab -P -R ${hostgroup} -- '${command}'",
+        }
+      }
+      default: {
+        exec { "Run-local-fabric-task-${taskname}":
+          command => "fab ${taskname}",
+        }
+      }
+    }
+  }
+
+  case $node_name {
+    $deployment_node: {
+      run_fabric { 'install database':
+        hostgroup => 'control',
+      } ->
+      run_fabric { 'setup_database':
+        hostgroup => 'control',
+      }
+
+    }
+  }
+
+}
