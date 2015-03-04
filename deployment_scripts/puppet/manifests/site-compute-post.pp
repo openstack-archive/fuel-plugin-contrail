d62c8d12d5ec747b5e049fad52735e19d2f0214c
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-03-04 15:32:45 +0200

    Contrail controller nodes deployment stage 1
    This commit contains puppet classes which create testbed file
    for fabric tool and execute fab tasks on controller nodes.
    
    Change-Id: Ie434d327ee53a5564adde001785b54a50eb30ebc
    Co-Authored-By: Vyacheslav Struk (vstruk@mirantis.com)

--- deployment_scripts/puppet/manifests/site-compute-post.pp
+++ deployment_scripts/puppet/manifests/site-compute-post.pp
@@ -1,10 +1,19 @@
 include contrail
 
+class { 'apt': disable_keys => true }
+
 class { 'contrail::network':
   node_role   => 'compute',
   address     => $contrail::address,
   ifname      => $contrail::ifname,
   netmask     => $contrail::netmask_short,
+} ->
+
+apt::source { 'contrail-from-fuel-master':
+  location     => "http://${contrail::master_ip}:8080/contrail-${contrail::plugin_version}/repositories/ubuntu/",
+  release      => '',
+  repos        => '/',
+  include_src  => false,
 } ->
 
 class { contrail::packages:
