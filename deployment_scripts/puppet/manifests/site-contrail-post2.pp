d83fa8dd843b8364d83cab187b9220b23d7db79d
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-03-10 19:12:45 +0200

    Contrail controller nodes deployment stage 2
    This commit contains puppet code which manages configuration files,
    installs the opencontrail neutron plugin and restarts services.
    
    Co-Authored-By: Vyacheslav Struk (vstruk@mirantis.com)
    Change-Id: Ibb9126d0e1493ae3c6d22bf7315c27656d1d8c43

--- deployment_scripts/puppet/manifests/site-contrail-post2.pp
+++ deployment_scripts/puppet/manifests/site-contrail-post2.pp
@@ -1,4 +1,13 @@
 include contrail
-if $contrail::node_name == $contrail::deployment_node {
-  
+if $contrail::node_name =~ /^#${contrail::contrail_node_basename}.\d+$/ {
+  class { 'contrail::neutron':
+    revision => $contrail::neutron_plugin_revision,
+  } ->
+  class { 'contrail::config':
+    node_role => $contrail::node_role,
+    node_name => $contrail::node_name,
+  } ~>
+  class { 'contrail::service':
+    node_role => $contrail::node_role,
+  }
 }
