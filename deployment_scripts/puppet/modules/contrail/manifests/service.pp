1c2e28ff4c009e396632a46b54d05b012688390c
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-03-10 19:12:45 +0200

    Contrail controller nodes deployment stage 2
    This commit contains puppet code which manages configuration files,
    installs the opencontrail neutron plugin and restarts services.
    Part of MOS controller configuration also implemented here.
    
    Change-Id: Ibb9126d0e1493ae3c6d22bf7315c27656d1d8c43

---
+++ deployment_scripts/puppet/modules/contrail/manifests/service.pp
@@ -0,0 +1,25 @@
+class contrail::service ( $node_role ) {
+
+  case $node_role {
+    'base-os': {
+      $services = $operatingsystem ? {
+        Ubuntu  => ['haproxy','keepalived','neutron-server'],
+        default => undef,
+      }
+    }
+    'controller','primary_controller': {
+      $services = $operatingsystem ? {
+        Ubuntu  => ['nova-api','nova-scheduler','nova-conductor'],
+        default => undef,
+      }
+    }
+  }
+
+  if ( $services ) {
+    service { $services:
+      ensure => running,
+      enable => true,
+    }
+  }
+
+}
