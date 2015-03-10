d194f6a8a2510874d04d8b2b50bf5e6e93703d8f
Vyacheslav Struk <vstruk@mirantis.com>
2015-03-10 16:59:46 +0300

    Various small fixes
    Change-Id: I05584c89b7bcdce8804366568ee4b93ad48049c4

--- deployment_scripts/puppet/modules/contrail/manifests/init.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/init.pp
@@ -17,13 +17,13 @@ $nodes= hiera('nodes')
 # Network configuration
 prepare_network_config($network_scheme)
 $ifname = get_private_ifname()
-$range_first = $settings['contrail_private_start']
-$range_last = $settings['contrail_private_end']
+$private_first = $settings['contrail_private_start']
+$private_last = $settings['contrail_private_end']
 # "cidr" is actually a network address with subnet, i.e. 192.168.150.0/24
 $cidr = $settings['contrail_private_cidr']
 $netmask=cidr_to_netmask($cidr) # returns i.e. "255.255.255.0"
 $netmask_short=netmask_to_cidr($netmask) # returns i.e. "/24"
-$address=get_ip_from_range($range_first,$range_last,$netmask_short,$uid,'first')
+$address=get_ip_from_range($private_first,$private_last,$netmask_short,$uid,'first')
 
 # Public address
 $neutron_settings=hiera('quantum_settings')
@@ -35,6 +35,8 @@ $public_prefix=$public_tmp[1] # netmask prefix here
 $public_addr=get_ip_from_range($public_first,$public_last,$public_prefix,$uid,'last')
 
 $public_if=$settings['contrail_public_if']
+
+$contrail_mgmt_vip=get_last_ip(get_network_role_property('management', 'cidr'))
 
 $contrail_node_basename='contrail'
 $deployment_node="${contrail_node_basename}-1"
