dddc1656f0e9334652e75bafa91f5f9f2d4ce373
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-03-18 18:22:03 +0200

    Neutron URL fixed
    A small fix for config puppet class, which resolves the neutron url issue.
    
    Change-Id: I2a391f258825cd449f25cfe9a14031e399979b6d

--- deployment_scripts/puppet/modules/contrail/manifests/config.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/config.pp
@@ -1,23 +1,23 @@
 class contrail::config ( $node_role ) {
   case $node_role {
-    'controller','primary_controller': {
+    'controller','primary-controller': {
       nova_config {
         'DEFAULT/network_api_class': value=> 'nova.network.neutronv2.api.API';
-        'DEFAULT/neutron_url': value => "http://${contrail::public_last}:9696";
+        'DEFAULT/neutron_url': value => "http://${contrail::contrail_mgmt_vip}:9696";
         'DEFAULT/neutron_admin_tenant_name': value=> 'services';
         'DEFAULT/neutron_admin_username': value=> 'neutron';
         'DEFAULT/neutron_admin_password': value=> $contrail::keystone['admin_token'];
         'DEFAULT/neutron_url_timeout': value=> '300';
         'DEFAULT/neutron_admin_auth_url': value=> "http://${contrail::mos_mgmt_vip}:35357/v2.0/";
         'DEFAULT/firewall_driver': value=> 'nova.virt.firewall.NoopFirewallDriver';
-        'DEFAULT/enabled_apis': value=> 'ec2;osapi_compute;metadata';
+        'DEFAULT/enabled_apis': value=> 'ec2,osapi_compute,metadata';
         'DEFAULT/security_group_api': value=> 'neutron';
         'DEFAULT/service_neutron_metadata_proxy': value=> 'True';
       }
     }
     'compute': {
       nova_config {
-        'DEFAULT/neutron_url': value => "http://${contrail::public_last}:9696";
+        'DEFAULT/neutron_url': value => "http://${contrail::contrail_mgmt_vip}:9696";
         'DEFAULT/neutron_admin_auth_url': value=> "http://${contrail::mos_mgmt_vip}:35357/v2.0/";
         'DEFAULT/network_api_class': value=> 'nova_contrail_vif.contrailvif.ContrailNetworkAPI';
         'DEFAULT/neutron_admin_tenant_name': value=> 'services';
