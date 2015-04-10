7a66de072d036032f4b4912a2c096d8e8f3a6799
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-04-10 16:45:10 +0300

    Plugin compartibility fixes
    Manifests and templates updated to ensure plugin tasks
    will be run only on nodes with contrail user names.
    
    Change-Id: I67a6c3668b5bec0c8efbe430828aec2ed8d5cf18

--- deployment_scripts/puppet/manifests/site-contrail-pre.pp
+++ deployment_scripts/puppet/manifests/site-contrail-pre.pp
@@ -1,37 +1,41 @@
 include contrail
 
-case $operatingsystem
-  {
-    Ubuntu:
-      {
-        $pkgs = ['python-crypto','python-netaddr','python-paramiko','ifenslave-2.6','patch',
+if $contrail::node_name =~ /^contrail.\d+$/ {
+
+  case $operatingsystem
+    {
+      Ubuntu:
+        {
+          $pkgs = ['python-crypto','python-netaddr','python-paramiko','ifenslave-2.6','patch',
                   'openjdk-7-jre-headless','contrail-fabric-utils','contrail-setup']
-        $pip_pkgs = ['ecdsa-0.10','Fabric-1.7.0']
-        }
-    CentOS:
-      {
-        $pkgs = ['python-netaddr','python-paramiko','patch',
+          $pip_pkgs = ['ecdsa-0.10','Fabric-1.7.0']
+          }
+      CentOS:
+        {
+          $pkgs = ['python-netaddr','python-paramiko','patch',
                   'java-1.7.0-openjdk','contrail-fabric-utils','contrail-setup']
-        $pip_pkgs = ['Fabric-1.7.0']
-      }
+          $pip_pkgs = ['Fabric-1.7.0']
+        }
+    }
+
+  class { 'contrail::network':
+    node_role       => 'base-os',
+    address         => $contrail::address,
+    ifname          => $contrail::ifname,
+    netmask         => $contrail::netmask_short,
+    public_addr     => $contrail::public_addr,
+    public_netmask  => $contrail::public_netmask,
+    public_if       => $contrail::public_if,
+    public_gw       => $contrail::public_gw
+  } ->
+
+  class { 'contrail::ssh':
+    password_auth => 'yes',
+    root_login    => 'yes'
+  } ->
+  class { 'contrail::package':
+    install        => $pkgs,
+    pip_install    => $pip_pkgs,
   }
 
-class { 'contrail::network':
-  node_role       => 'base-os',
-  address         => $contrail::address,
-  ifname          => $contrail::ifname,
-  netmask         => $contrail::netmask_short,
-  public_addr     => $contrail::public_addr,
-  public_netmask  => $contrail::public_netmask,
-  public_if       => $contrail::public_if,
-  public_gw       => $contrail::public_gw
-} ->
-
-class { 'contrail::ssh':
-  password_auth => 'yes',
-  root_login => 'yes'
-} ->
-class { 'contrail::package':
-  install        => $pkgs,
-  pip_install    => $pip_pkgs,
 }
