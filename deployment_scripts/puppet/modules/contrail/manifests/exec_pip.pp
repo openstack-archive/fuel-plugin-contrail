a7b9692bb9fbb1b71debf26c4c7ad92e0168ea2f
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-05-05 20:45:39 +0300

    Package priorities added
    The puppet manifest to adjust repository and package priorities
    on Ubuntu and CentOS added.
    
    Change-Id: I47c6c281c613f5553d75e663dc115901c7602fbc

--- deployment_scripts/puppet/modules/contrail/manifests/exec_pip.pp
+++
@@ -1,7 +0,0 @@
-# A helper to run pip
-define contrail::exec_pip ( $path ){
-  exec { "Install-pip-package-${name}":
-    path    => '/usr/local/bin/:/usr/bin:/bin',
-    command => "pip install --upgrade --no-deps --index-url='' ${path}/${name}.tar.gz",
-  }
-}
