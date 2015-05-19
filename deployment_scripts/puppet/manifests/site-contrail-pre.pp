b1d6ebd0f3b232eae7f5767f131264130bc1dc10
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-05-19 18:46:05 +0300

    Change addressing FPL-201
    Change-Id: I03a05f4597635a87a7133ec4aa61bd28b2971b38

--- deployment_scripts/puppet/manifests/site-contrail-pre.pp
+++ deployment_scripts/puppet/manifests/site-contrail-pre.pp
@@ -18,40 +18,17 @@ Exec { path => '/bin:/sbin:/usr/bin:/usr/sbin'}
 
 if $contrail::node_name =~ /^contrail.\d+$/ {
 
-  case $operatingsystem
-    {
-      Ubuntu:
-        {
-          $pkgs = ['python-crypto','python-netaddr','python-paramiko','ifenslave-2.6','patch',
-                  'openjdk-7-jre-headless','contrail-fabric-utils','contrail-setup']
-          $pip_pkgs = ['ecdsa-0.10','Fabric-1.7.0']
-          }
-      CentOS:
-        {
-          $pkgs = ['python-netaddr','python-paramiko','patch',
-                  'java-1.7.0-openjdk','contrail-fabric-utils','contrail-setup']
-          $pip_pkgs = ['Fabric-1.7.0']
-        }
-    }
-
   class { 'contrail::network':
-    node_role       => $node_role,
-    address         => $contrail::address,
-    ifname          => $contrail::ifname,
-    netmask         => $contrail::netmask_short,
-    public_addr     => $contrail::public_addr,
-    public_netmask  => $contrail::public_netmask,
-    public_if       => $contrail::public_if,
-    public_gw       => $contrail::public_gw,
+    node_role  => $node_role,
+    address    => $contrail::address,
+    ifname     => $contrail::ifname,
+    netmask    => $contrail::netmask_short,
+    default_gw => $contrail::default_gw,
   } ->
 
   class { 'contrail::ssh':
     password_auth => 'yes',
     root_login    => 'yes'
-  } ->
-  class { 'contrail::package':
-    install        => $pkgs,
-    pip_install    => $pip_pkgs,
   }
 
 }
