74fcc2d2bf315b37119e9cd6567be0b1935125c4
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-05-21 21:11:39 +0300

    OpenVSwitch removal routines updated
    This commit is addressing the new openvswitch-dkms and openvswitch-kmod packages in
    newer Fuel ISOs.
    
    Change-Id: I52a8ce442b5e00f8eb1cc6c2f3362bbd50bf5635

--- deployment_scripts/puppet/manifests/site-compute-post.pp
+++ deployment_scripts/puppet/manifests/site-compute-post.pp
@@ -28,14 +28,14 @@ case $operatingsystem {
 
     class { 'contrail::package':
       install => ['contrail-openstack-vrouter','contrail-vrouter-dkms','iproute2','haproxy','libatm1'],
-      remove  => ['openvswitch-common','openvswitch-datapath-lts-saucy-dkms','openvswitch-switch','nova-network','nova-api'],
+      remove  => ['openvswitch-common','openvswitch-datapath-dkms','openvswitch-datapath-lts-saucy-dkms','openvswitch-switch','nova-network','nova-api'],
       }
 
   }
   CentOS: {
     class { 'contrail::package':
       install => ['contrail-openstack-vrouter','iproute','haproxy','patch'],
-      remove  => ['openvswitch','openstack-neutron-openvswitch'],
+      remove  => ['openvswitch','kmod-openvswitch','kmod-openvswitch-lt','openstack-neutron-openvswitch'],
     }
     ->
     class { 'contrail::vrouter_module':}
