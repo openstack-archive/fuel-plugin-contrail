bc14607410d4fd6a83ff8c9eca1ea3a3ea7fa6e9
Vyacheslav Struk <vstruk@mirantis.com>
2015-06-22 19:10:53 +0300

    A workaround that allows to complete re-deploy
    Network configuration substituted by noop manifest.
    
    Change-Id: I2cf35bc5018f1985b527eb36053c013041b58065

--- deployment_scripts/puppet/manifests/site-common-pre.pp
+++ deployment_scripts/puppet/manifests/site-common-pre.pp
@@ -12,6 +12,20 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+Exec { path => '/bin:/sbin:/usr/bin:/usr/sbin', refresh => 'echo NOOP_ON_REFRESH'}
+
+# Temporary dirty hack. Network configuration fails because of deployed contrail [FIXME]
+############################################################################
+exec {'no_predefined_network_hack':
+  command => 'sed -i -e "82s/.*/\tif\ false\ {/" /etc/puppet/modules/osnailyfacter/modular/openstack-network/openstack-network-controller.pp'
+}
+
+exec {'no_network_reconfigure':
+  command => 'echo "#NOOP here. Modified by contrail plugin" > /etc/puppet/modules/osnailyfacter/modular/openstack-network/openstack-network-compute.pp',
+  onlyif => 'test -f /opt/contrail/provision-vrouter-DONE'
+}
+############################################################################
+
 case $operatingsystem
 {
     CentOS:
