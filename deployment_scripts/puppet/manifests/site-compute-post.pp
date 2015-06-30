e227f7bc38665ebed9e433f823c89fab71422145
Vyacheslav Struk <vstruk@mirantis.com>
2015-06-30 16:07:32 +0300

    Updates to bonding support for Contrail controllers
    get_private_ifname function updated to return a correct interface name
    for environments with bonding enabled on Contrail controllers.
    
    Change-Id: I5e1d8e79fef0c80ac642e21bf6fbc47a12196733

--- deployment_scripts/puppet/manifests/site-compute-post.pp
+++ deployment_scripts/puppet/manifests/site-compute-post.pp
@@ -28,7 +28,7 @@ case $operatingsystem {
     file { '/etc/apt/preferences.d/contrail-pin-100':
       ensure  => file,
       content => template('contrail/contrail-pin-100.erb'),
-     }
+    }
     class { 'contrail::package':
       install => ['contrail-openstack-vrouter','contrail-vrouter-dkms','iproute2','haproxy','libatm1'],
       remove  => ['openvswitch-common','openvswitch-datapath-dkms','openvswitch-datapath-lts-saucy-dkms','openvswitch-switch','nova-network','nova-api'],
