d0e9aa7ae4e6b453658b0bd0805ddda0295fc79b
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-08-18 19:43:45 +0300

    Contrail HA puppet manifest added
    This commit contains puppet code, files and templates
    to deploy the software packages and configuration
    for keepalived and haproxy for contrail HA.
    
    Change-Id: If4c48ba142607a9ed4221d286c6ef4799fbaadf1

---
+++ deployment_scripts/puppet/modules/contrail/manifests/vip.pp
@@ -0,0 +1,52 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+class contrail::vip {
+
+  Package {
+    ensure => installed,
+  }
+  File {
+    ensure  => present,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+  }
+
+# Packages
+  package { 'keepalived': } ->
+  package { 'haproxy': }
+
+# Configs
+  file { '/etc/keepalived/keepalived.conf':
+    content => template('contrail/keepalived.conf.erb'),
+  }
+  file { '/etc/haproxy/haproxy.conf':
+    content => template('contrail/haproxy.conf.erb'),
+  }
+
+# Services
+  service { 'keepalived':
+    ensure    => running,
+    enable    => true,
+    require   => Package['keepalived'],
+    subscribe => File['/etc/keepalived/keepalived.conf'],
+  }
+  service { 'haproxy':
+    ensure    => running,
+    enable    => true,
+    require   => Package['haproxy'],
+    subscribe => File['/etc/haproxy/haproxy.conf'],
+  }
+
+}
