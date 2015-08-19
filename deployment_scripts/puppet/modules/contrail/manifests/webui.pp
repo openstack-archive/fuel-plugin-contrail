992366fced6d3811c20ba023d47fe767ebf20e8e
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-08-19 19:31:07 +0300

    Puppet webui class added
    This commit contains puppet code, files and templates
    to deploy the software packages and configuration
    for contrail webui service.
    
    Change-Id: I5b4d5c06af9178d0fb4303e90f01b865c1294ed8

---
+++ deployment_scripts/puppet/modules/contrail/manifests/webui.pp
@@ -0,0 +1,55 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+class contrail::webui {
+
+# Resources defaults
+  Package { ensure => present }
+
+  File {
+    ensure  => present,
+    mode    => '0644',
+    owner   => root,
+    group   => root,
+    require => Package['contrail-openstack-webui'],
+  }
+
+  Exec { path => '/usr/bin:/usr/sbin:/bin:/sbin' }
+
+# Packages
+  package { 'nodejs': } ->
+  package { 'contrail-web-core': } ->
+  package { 'contrail-web-controller': } ->
+  package { 'contrail-openstack-webui': }
+
+# Webui config files
+  file { '/etc/contrail/config.global.js':
+    content => template('contrail/config.global.js.erb'),
+  }
+
+  file { '/etc/contrail/contrail-webui-userauth.js':
+    content => template('contrail/contrail-webui-userauth.js.erb'),
+  }
+
+# Services
+  service { 'supervisor-webui':
+    ensure      => running,
+    enable      => true,
+    require     => Package['contrail-openstack-webui'],
+    subscribe   => [File['/etc/contrail/contrail-webui-userauth.js'],
+                    File['/etc/contrail/config.global.js'],
+                    ],
+  }
+
+}
