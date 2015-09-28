732483b3a0f797a4967697ec77a738190d546d62
Michal Rostecki <mrostecki@mirantis.com>
2015-09-28 20:10:42 +0200

    [WIP] Transfer Contrail logs via syslog
    Add cron job for transfer contrail-logs to Fuel
    master via syslog.
    
    Change-Id: I2ee93e11a5fed5b90af12e8bc3b66076d60cfab9
    Closes-Bug: #1499314

--- deployment_scripts/puppet/modules/contrail/manifests/analytics.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/analytics.pp
@@ -83,4 +83,25 @@ class contrail::analytics {
                     ],
   }
 
+  file { 'contrailsyslog.sh':
+    ensure  => 'present',
+    path    => '/usr/local/sbin/contrailsyslog.sh',
+    mode    => '0700',
+    owner   => 'root',
+    group   => 'root',
+    content => template('contrail/contrailsyslog.sh.erb'),
+  }
+
+  cron { 'contrail-syslog':
+    ensure   => 'present',
+    command  => '/usr/local/sbin/contrailsyslog.sh',
+    user     => 'root',
+    minute   => '*/1',
+    provider => 'crontab',
+    require  => [
+      Service['supervisor-analytics'],
+      File['contrailsyslog.sh'],
+    ],
+  }
+
 }
