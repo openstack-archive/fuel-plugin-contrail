df0ae6078dbe22764fc8ed819c3a4f5bbb9e3859
Oleksandr Kosse <okosse@mirantis.com>
2015-10-21 12:37:00 +0000

    Add logger
    Change-Id: Ib58d3197ecb13d731711057cfc88ce68085964df

--- plugin_test/run_tests.py
+++ plugin_test/run_tests.py
@@ -13,7 +13,8 @@
 #    under the License.
 
 import sys
-
+import functools
+import logging
 import os
 import re
 from nose.plugins import Plugin
@@ -56,6 +57,59 @@ def run_tests():
 
 if __name__ == '__main__':
     sys.path.append(sys.path[0]+"/fuel-qa")
+
+
+    from fuelweb_test.settings import LOGS_DIR
+    if not os.path.exists(LOGS_DIR):
+            os.makedirs(LOGS_DIR)
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s - %(levelname)s %(filename)s:'
+                        '%(lineno)d -- %(message)s',
+                        filename=os.path.join(LOGS_DIR, 'sys_test.log'),
+                        filemode='w')
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s %(filename)s:'
+                                  '%(lineno)d -- %(message)s')
+    console.setFormatter(formatter)
+    logger = logging.getLogger(__name__)
+    logger.addHandler(console)
+    class NoDebugMessageFilter(logging.Filter):
+        def filter(self, record):
+            return not record.levelno <= logging.DEBUG
+    
+    logging.getLogger('paramiko.transport').addFilter(NoDebugMessageFilter())
+    logging.getLogger('paramiko.hostkeys').addFilter(NoDebugMessageFilter())
+    logging.getLogger('iso8601.iso8601').addFilter(NoDebugMessageFilter())
+
+    def debug(logger):
+        def wrapper(func):
+            @functools.wraps(func)
+            def wrapped(*args, **kwargs):
+                logger.debug(
+                    "Calling: {} with args: {} {}".format(
+                         func.__name__, args, kwargs
+                    )
+                )
+                result = func(*args, **kwargs)
+                logger.debug(
+                    "Done: {} with result: {}".format(func.__name__, result))
+                return result
+            return wrapped
+        return wrapper
+    logwrap = debug(logger)
+    class quiet_logger(object):
+         """Reduce logging level while context is executed."""
+
+         def __enter__(self):
+             console.setLevel(logging.ERROR)
+
+         def __exit__(self, exp_type, exp_value, traceback):
+             console.setLevel(logging.INFO)
+
+
+
+
     import_tests()
     from fuelweb_test.helpers.patching import map_test
     if any(re.search(r'--group=patching_master_tests', arg)
