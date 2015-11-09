c379e0dc5d089199a6d378c19be8debdc0e02020
Illia Polliul <ipolliul@mirantis.com>
2015-11-09 20:29:15 +0200

    [WIP] Deployment tasks rewritten to support custom roles. Since Contrail controller components are not nessesarly on one node, tasks also need to be split. Also, templates updated with new ip addresses. Change-Id: I236085c86dc18fe53f6a1d59dbcfafb170d64c18

---
+++ deployment_scripts/puppet/manifests/contrail-utils.pp
@@ -0,0 +1,51 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+notice('MODULAR: contrail/contrail-utils.pp')
+
+include contrail
+
+class { 'contrail::network':
+  node_role  => $node_role,
+  address    => $contrail::address,
+  ifname     => $contrail::phys_dev,
+  netmask    => $contrail::netmask_short,
+  default_gw => $contrail::default_gw,
+}
+
+case $operatingsystem {
+  Ubuntu: {
+    exec { 'reinstall-tzdata':
+      path    => '/bin:/sbin:/usr/bin:/usr/sbin',
+      command => '/usr/bin/apt-get install -y --force-yes tzdata',
+      before  => Class['contrail::package'],
+    }
+    $pkgs = ['python-crypto','python-netaddr','python-paramiko',
+      'ifenslave-2.6','patch','openjdk-7-jre-headless',
+      'python-contrail','contrail-setup','contrail-utils','contrail-nodemgr','supervisor']
+  }
+
+  CentOS: {
+    $pkgs     = ['python-netaddr','python-paramiko','patch',
+      'java-1.7.0-openjdk','contrail-fabric-utils','contrail-setup']
+    $pip_pkgs = ['Fabric-1.7.5']
+  }
+
+  default: {}
+}
+
+class { 'contrail::package':
+  install     => $pkgs,
+  pip_install => $pip_pkgs,
+}
