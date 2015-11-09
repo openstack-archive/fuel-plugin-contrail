bcf8dc8d6b45fb678605ee87d610a0873d8edb5a
Illia Polliul <ipolliul@mirantis.com>
2015-11-09 20:29:15 +0200

    [WIP] Deployment tasks rewritten to support custom roles. Since Contrail controller components are not nessesarly on one node, tasks also need to be split. Also, templates updated with new ip addresses. Change-Id: I236085c86dc18fe53f6a1d59dbcfafb170d64c18

---
+++ deployment_scripts/puppet/manifests/contrail-repo.pp
@@ -0,0 +1,39 @@
+#    Copyright 2015 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+notice('MODULAR: contrail/contrail-repo.pp')
+
+case $operatingsystem
+{
+    CentOS:
+      {
+        yumrepo {'mos': priority => 1, exclude => 'python-thrift,nodejs'} # Contrail requires newer python-thrift and nodejs from it's repo
+        package {'yum-plugin-priorities': ensure => present }
+      }
+    Ubuntu: {
+      file { '/etc/apt/preferences.d/contrail-3.0.0.pref':
+        ensure => absent,
+      }
+      case $::role {
+        /^contrail/: {
+          file { '/etc/apt/preferences.d/contrail-pin-100':
+            ensure => file,
+            source => 'puppet:///modules/contrail/contrail-pin-100',
+          }
+        }
+        default: {}
+      }
+    }
+    default: {}
+}
