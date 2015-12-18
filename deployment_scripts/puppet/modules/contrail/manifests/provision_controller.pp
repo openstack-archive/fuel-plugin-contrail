4b72d91f76ded85067bb70b1aac36c86c9ebcd2a
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-12-18 10:33:38 +0200

    Contrail plugin for Fuel 9.0
    - components.yaml file added for component registry feature
    - install.sh script updated with new package names
    - hiera overrides updated
    - neutron-related tasks updated
    
    Change-Id: I34f046a743c9065275cf124ac3ef888139c8580c

--- deployment_scripts/puppet/modules/contrail/manifests/provision_controller.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/provision_controller.pp
@@ -14,18 +14,19 @@
 
 class contrail::provision_controller {
 
-contrail::create_network{'net04':
-  netdata          => $contrail::nets['net04'],
+contrail::create_network{$contrail::private_net:
+  netdata  => $contrail::nets[$contrail::private_net],
 } ->
 
-contrail::create_network{'net04_ext':
-  netdata => $contrail::nets['net04_ext'],
+contrail::create_network{$contrail::floating_net:
+  netdata => $contrail::nets[$contrail::floating_net],
   notify  => Exec['prov_route_target'],
 } ->
 
-openstack::network::create_router{'router04':
-  internal_network => 'net04',
-  external_network => 'net04_ext',
+neutron_router{$contrail::default_router:
+  ensure           => present,
+  internal_network => $contrail::private_net,
+  external_network => $contrail::floating_net,
   tenant_name      => $contrail::admin_tenant
 }
 
@@ -33,13 +34,13 @@ exec { 'prov_route_target':
   provider => 'shell',
   path     => '/usr/bin:/bin:/sbin',
   command  => "python /usr/share/contrail-utils/add_route_target.py \
---routing_instance_name default-domain:${contrail::admin_tenant}:net04_ext:net04_ext \
+--routing_instance_name default-domain:${contrail::admin_tenant}:${contrail::floating_net}:${contrail::floating_net} \
 --route_target_number ${contrail::route_target} --router_asn ${contrail::asnum} \
 --api_server_ip ${contrail::contrail_mgmt_vip} --api_server_port 8082 \
 --admin_user neutron --admin_tenant_name services --admin_password '${contrail::service_token}' \
 && touch /etc/contrail/prov_route_target-DONE",
   creates  => '/etc/contrail/prov_route_target-DONE',
-  require  => Contrail::Create_Network['net04_ext'],
+  require  => Contrail::Create_Network[$contrail::floating_net],
 }
 
 }
