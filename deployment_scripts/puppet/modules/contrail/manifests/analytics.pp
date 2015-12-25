d82d9b7d2def4089953b008f50ae43c05312676d
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2015-12-25 17:34:33 +0200

    Contrail syslog script updated
    - analytics_api_ip argument added to contrail-logs command
    - cron job now is executed on primary config node only
    
    Change-Id: I0aa8f32dde2acfd86ab4bd4d1b436c2356f69591

--- deployment_scripts/puppet/modules/contrail/manifests/analytics.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/analytics.pp
@@ -97,23 +97,26 @@ class contrail::analytics {
   }
 
   # Cron job for transfer contrail-logs to Fuel master
-  file { 'contrailsyslog.sh':
-    ensure  => 'present',
-    path    => '/usr/local/sbin/contrailsyslog.sh',
-    mode    => '0700',
-    owner   => 'root',
-    group   => 'root',
-    content => template('contrail/contrailsyslog.sh.erb'),
-  }
+  # Runs on primary analytics node
+  if $contrail::node_role == 'primary-contrail-config' {
+    file { 'contrailsyslog.sh':
+      ensure  => 'present',
+      path    => '/usr/local/sbin/contrailsyslog.sh',
+      mode    => '0700',
+      owner   => 'root',
+      group   => 'root',
+      content => template('contrail/contrailsyslog.sh.erb'),
+    }
 
-  cron { 'contrail-syslog':
-    command => '/usr/local/sbin/contrailsyslog.sh',
-    user    => 'root',
-    minute  => '*/1',
-    require => [
-      Service['supervisor-analytics'],
-      File['contrailsyslog.sh'],
-    ],
+    cron { 'contrail-syslog':
+      command => '/usr/local/sbin/contrailsyslog.sh',
+      user    => 'root',
+      minute  => '*/1',
+      require => [
+        Service['supervisor-analytics'],
+        File['contrailsyslog.sh'],
+      ],
+    }
   }
 
 }
