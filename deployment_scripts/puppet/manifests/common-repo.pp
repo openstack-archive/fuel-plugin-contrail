7a50d4f6a2c2c84f197373a03fc98df1181b0e6d
Szymon Florczak <sflorczak@mirantis.com>
2016-02-25 10:36:14 +0100

    Changing the plugin version number
    Change is required to help with Contrail upgrades.
    
    Change-Id: Ie401084abb9ce37140854822ed57166d666851f7

--- deployment_scripts/puppet/manifests/common-repo.pp
+++ deployment_scripts/puppet/manifests/common-repo.pp
@@ -21,7 +21,7 @@ case $operatingsystem
       package {'yum-plugin-priorities': ensure => present }
     }
     Ubuntu: {
-      file { '/etc/apt/preferences.d/contrail-3.0.0.pref':
+      file { '/etc/apt/preferences.d/contrail-2.2.0.pref':
         ensure => absent,
       }
     }
