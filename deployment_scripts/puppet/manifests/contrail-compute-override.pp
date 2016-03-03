78f552e8dee4e2c90c3cbcf680087d963a4aa460
Vitalii Kovalchuk <vkovalchuk@mirantis.com>
2016-03-03 16:14:15 +0000

    Fix deployment tasks order, fix for contrail analytics dependency
    Change-Id: I3fb6e9332bad589130176eb02bfe58270816c408

---
+++ deployment_scripts/puppet/manifests/contrail-compute-override.pp
@@ -0,0 +1,34 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+notice('MODULAR: contrail/contrail-compute-override.pp')
+
+include contrail
+
+$common_pkg = ['iproute2', 'haproxy', 'libatm1']
+$nova_pkg   = ['nova-compute', 'nova-common', 'python-nova', 'python-urllib3']
+
+if $contrail::compute_dpdk_enabled and $contrail::install_contrail_nova {
+  $override_pkg = concat($common_pkg, $nova_pkg)
+} else {
+  $override_pkg = $common_pkg
+}
+
+apt::pin { 'contrail-override':
+  explanation => 'Set priority for packages that need to override from contrail repository',
+  priority    => 1200,
+  label       => 'contrail',
+  packages    => $override_pkg,
+}
+
