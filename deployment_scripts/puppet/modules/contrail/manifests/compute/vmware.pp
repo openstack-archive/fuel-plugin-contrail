e857b9f13c0255cfeacd12452ae3a2026c891544
Alexander Arzhanov <aarzhanov@mirantis.com>
2016-03-05 00:04:52 +0300

    Compute-vmware node role deployment
    - installation of vcenter-specific contrail packages
    - configuration for contrail-vcenter-plugin
    - bugfix permissions on package contrail-vcenter-plugin
    - install contrail-utils
    
    Change-Id: Ib2ebf575dbbf52f7506ffe6aed64e33caa6d1b3e

---
+++ deployment_scripts/puppet/modules/contrail/manifests/compute/vmware.pp
@@ -0,0 +1,74 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+class contrail::compute::vmware (
+  $vcenter_computes,
+  ) {
+
+  $vc_host = $vcenter_computes[0]['vc_host']
+  $vc_user = $vcenter_computes[0]['vc_user']
+  $vc_password = $vcenter_computes[0]['vc_password']
+
+# Install vCenter-specific contrail packages
+  package { ['libxml-commons-external-java', 'libxml-commons-resolver1.1-java', 'libxerces2-java',
+            'libslf4j-java', 'libnetty-java', 'libjline-java', 'libzookeeper-java']: } ->
+  package { 'contrail-install-vcenter-plugin': } ->
+  package { ['libcontrail-java-api','libcontrail-vijava','libcontrail-vrouter-java-api']: } ->
+  package { 'contrail-vcenter-plugin': }
+
+# Add user and group
+  group { 'contrail':
+     ensure => 'present',
+     }
+
+  user { 'contrail':
+    ensure => 'present',
+    home   => '/var/lib/contrail',
+    groups => 'contrail',
+    shell  => '/bin/false',
+    require => Group['contrail'],
+    }
+
+# Config file
+  file { '/etc/contrail':
+    ensure  => directory,
+    mode    => '0750',
+    owner   => 'contrail',
+    group   => 'contrail',
+    require => User['contrail'],
+  }
+
+  file {'/etc/contrail/contrail-vcenter-plugin.conf':
+    ensure  => present,
+    content => template('contrail/contrail-vcenter-plugin.conf.erb'),
+    require => [Package['contrail-vcenter-plugin'],File['/etc/contrail']],
+  }
+
+#### Todo ####
+### Need create file /etc/contrail/ESXiToVRouterIp.map ###################
+#  file {'/etc/contrail/ESXiToVRouterIp.map':
+#    ensure  => present,
+#    content => template('contrail/ESXiToVRouterIp.map.erb'),
+#    require => [Package['contrail-vcenter-plugin'],File['/etc/contrail']],
+#  }
+### Need subscribe service { 'contrail-vcenter-plugin': ##################
+
+# Enable and start service
+  service { 'contrail-vcenter-plugin':
+    ensure    => running,
+    enable    => true,
+    subscribe => [Package['contrail-vcenter-plugin'],File['/etc/contrail/contrail-vcenter-plugin.conf']],
+  }
+}
+
