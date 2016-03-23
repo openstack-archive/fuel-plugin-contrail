335a1fe5007bda70c7ebb06b74c5284f35d4efcc
Illia Polliul <ipolliul@mirantis.com>
2016-03-23 16:59:22 +0200

    Fix for failing Ceph
    Ceph manifest is failing on compute beacuse it uses role instead of roles.
    This will supposably fixed in MU, but temporary workaround required.
    
    Change-Id: Ice754a4b722321f79f76743852954a5b0f852c1e

---
+++ deployment_scripts/puppet/modules/contrail/files/roles_include.rb
@@ -0,0 +1,19 @@
+module Puppet::Parser::Functions
+  newfunction(
+      :roles_include,
+      :type => :rvalue,
+      :arity => 1,
+      :doc => <<-EOS
+Check if this node's roles include these roles.
+EOS
+  ) do |args|
+    raise Puppet::ParseError, 'Only one argument with role or array of roles should be provided!' if args.size != 1
+    intended_roles = args.first
+    intended_roles = [intended_roles] unless intended_roles.is_a? Array
+    network_metadata = function_hiera_hash ['network_metadata']
+    node_name = function_get_node_key_name []
+    node_roles = network_metadata.fetch('nodes', {}).fetch(node_name, {}).fetch('node_roles', [])
+
+    (node_roles & intended_roles).any?
+  end
+end
