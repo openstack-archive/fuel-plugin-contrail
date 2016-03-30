5a88836596d4fc06b417c641207af2300c2d4f25
Illia Polliul <ipolliul@mirantis.com>
2016-03-30 14:51:33 +0300

    QEMU pin from dpdk-depends
    Change-Id: I5397a32e8f43b04d0e7ce17034694cd558319f4d

--- deployment_scripts/puppet/manifests/contrail-compute-repo.pp
+++ deployment_scripts/puppet/manifests/contrail-compute-repo.pp
@@ -27,6 +27,11 @@ apt::pin { 'contrail-main':
   label       => 'contrail',
 }
 
+Exec {
+  provider => 'shell',
+  path     => '/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin',
+}
+
 # Temporary dirty hack. Network configuration fails because of deployed contrail vrouter [FIXME]
 $osnailyfacter_path = '/etc/puppet/modules/osnailyfacter'
 
@@ -40,6 +45,7 @@ exec {'no_openstack_network_reconfigure':
   onlyif => '/usr/bin/test -f /opt/contrail/provision-vrouter-DONE'
 }
 
+# Temporary fix for ceph manifests from fuel-library
 file { "${osnailyfacter_path}/lib/puppet/parser/functions/roles_include.rb":
   source  => 'puppet:///modules/contrail/roles_include.rb',
 }
@@ -52,4 +58,20 @@ file_line { 'roles':
   path  => '/etc/puppet/modules/ceph/manifests/init.pp',
   line  => " if roles_include(['primary-controller', 'controller', 'ceph-mon', 'ceph-osd', 'compute', 'cinder']) {",
   match => 'controller\|ceph\|compute\|cinder',
+}
+
+if $contrail::compute_dpdk_enabled {
+  # Create local dpdk repository
+  package { 'dpdk-depends-packages':
+    ensure => present,
+  } ->
+  file { 'dpdk-label':
+    path    => '/opt/contrail/contrail_install_repo_dpdk/Release',
+    ensure  => file,
+    content => 'Label: dpdk-depends-packages',
+  } ->
+  exec {'setup_dpdk_repo':
+    command => 'bash /opt/contrail/contrail_packages_dpdk/setup.sh && touch /opt/contrail/dpdk-repo-DONE',
+    creates => '/opt/contrail/dpdk-repo-DONE',
+  }
 }
