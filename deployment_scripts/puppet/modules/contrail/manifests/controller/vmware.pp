0dd9433569d48795943a2cef8c3f975af76f79e4
Vitalii Kovalchuk <vkovalchuk@mirantis.com>
2016-04-11 18:21:46 +0300

    Start ContrailVM on esxi nodes
    - execute fabric task - prov_esxi
    - add possibility change environment type
    
    Change-Id: If5d24dcd6cac51c57b2800114259c64493168d67

--- deployment_scripts/puppet/modules/contrail/manifests/controller/vmware.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/controller/vmware.pp
@@ -15,34 +15,84 @@
 class contrail::controller::vmware {
 
   if $contrail::use_vcenter {
-    
+
+    if $contrail::env == 'dev' {
+      file_line { 'change_memsize1':
+        path    => '/opt/contrail/utils/fabfile/templates/compute_vmx_template.py',
+        line    => 'memsize = "2048"',
+        match   => '^memsize',
+        require => Class['contrail::package'],
+        before  => Exec['fab_prov_esxi'],
+      }
+
+      file_line { 'change_memsize2':
+        path    => '/opt/contrail/utils/fabfile/templates/compute_vmx_template.py',
+        line    => 'sched.mem.min = "2048"',
+        match   => '^sched\.mem\.min',
+        require => Class['contrail::package'],
+        before  => Exec['fab_prov_esxi'],
+      }
+
+      #This is temporary solution for development purposes
+      file { '/root/.ssh/id_rsa':
+        mode   => '0600',
+        source => 'puppet:///modules/contrail/vmware_priv',
+        before => Exec['fab_prov_esxi'],
+      }
+
+      file { '/root/.ssh/id_rsa.pub':
+        mode   => '0644',
+        source => 'puppet:///modules/contrail/vmware_pub',
+        before => Exec['fab_prov_esxi'],
+      }
+
+    }
+
     $pkgs = ['contrail-fabric-utils','contrail-setup']
     $pip_pkgs = ['Fabric-1.7.5']
 
     class { 'contrail::package':
       install     => $pkgs,
       pip_install => $pip_pkgs,
-    } ->
+    }
+
     exec {'retrive install packages':
       command => "/usr/bin/curl -fLO http://${::contrail::master_ip}:8080/plugins/contrail-3.0/latest-contrail-install-packages.deb",
       creates => '/opt/contrail/contrail-install-packages.deb',
-      cwd     => '/opt/contrail'
-    } ->
+      cwd     => '/opt/contrail',
+      before  => Exec['fab_prov_esxi'],
+      require => Class['contrail::package'],
+    }
 
     exec {'retrive vmware plugin packages':
       command => "/usr/bin/curl -fLO http://${::contrail::master_ip}:8080/plugins/contrail-3.0/latest-contrail-install-vcenter-plugin.deb",
       creates => '/opt/contrail/contrail-install-vcenter-plugin.deb',
-      cwd     => '/opt/contrail'
-    } ->
+      cwd     => '/opt/contrail',
+      before  => Exec['fab_prov_esxi'],
+      require => Class['contrail::package'],
+    }
 
     exec {'retrive vmdk':
       command => "/usr/bin/curl -fLO http://${::contrail::master_ip}:8080/plugins/contrail-3.0/ContrailVM-disk1.vmdk",
       creates => '/opt/contrail/ContrailVM-disk1.vmdk',
-      cwd     => '/opt/contrail'
-    } ->
+      cwd     => '/opt/contrail',
+      before  => Exec['fab_prov_esxi'],
+      require => Class['contrail::package'],
+    }
 
     file { '/opt/contrail/utils/fabfile/testbeds/testbed.py':
       content => template('contrail/vmware_testbed.py.erb'),
+      mode    => '0775',
+      before  => Exec['fab_prov_esxi'],
+      require => Class['contrail::package'],
     }
+
+    exec { 'fab_prov_esxi':
+        path    => '/usr/local/bin:/bin:/usr/bin/',
+        cwd     => '/opt/contrail/utils',
+        command => 'fab prov_esxi && touch /opt/contrail/fab_prov_esxi-DONE',
+        creates => '/opt/contrail/fab_prov_esxi-DONE',
+    }
+
   }
 }
