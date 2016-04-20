bc5060373df4c03865dd90b1c35c2c919ac015fa
Vitalii Kovalchuk <vkovalchuk@mirantis.com>
2016-04-21 00:26:21 +0300

    Integrate vmware with ContrailUI
    Change-Id: I9ff07bcd34664c46ae1cf5871ae75037d136ee89

--- deployment_scripts/puppet/modules/contrail/manifests/deliver_hiera.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/deliver_hiera.pp
@@ -12,7 +12,19 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-notice('MODULAR: contrail/contrail-controller-vmware.pp')
 
-include contrail
-class { 'contrail::controller::vmware': }
+define contrail::deliver_hiera (
+  $host = $title,
+  )
+{
+  exec {"create_override_dir_${host}":
+    path    => ['/bin', '/usr/bin'],
+    command => "ssh root@${host} 'mkdir -p /etc/hiera/override/'",
+  } ->
+
+  exec {"copy_yaml_to_config_node_${host}":
+    path    => ['/bin', '/usr/bin'],
+    command => "scp -oStrictHostKeyChecking=no -i /var/lib/astute/vmware/vmware /root/config-override.yaml root@${host}:/etc/hiera/override/plugins.yaml",
+  }
+
+}
