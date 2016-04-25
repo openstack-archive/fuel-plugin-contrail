75f718aac592e6a1a6bcd3a4e2c170206e803a11
Illia Polliul <ipolliul@mirantis.com>
2016-04-25 13:19:38 +0300

    [WIP] vCenter integration from 3.0.1 (Kilo) branch
    Change-Id: If340cb193568e7f7266ecf721cf8d7c9e7ecce4d

---
+++ deployment_scripts/puppet/modules/contrail/manifests/deliver_hiera.pp
@@ -0,0 +1,32 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+define contrail::deliver_hiera (
+  $host = $title,
+  )
+{
+  $opt='-oStrictHostKeyChecking=no -i /var/lib/astute/vmware/vmware'
+
+  exec {"create_override_dir_${host}":
+    path    => ['/bin', '/usr/bin'],
+    command => "ssh ${opt} root@${host} 'mkdir -p /etc/hiera/override/'",
+  } ->
+
+  exec {"copy_yaml_to_config_node_${host}":
+    path    => ['/bin', '/usr/bin'],
+    command => "scp ${opt} /root/config-override.yaml root@${host}:/etc/hiera/override/plugins.yaml",
+  }
+
+}
