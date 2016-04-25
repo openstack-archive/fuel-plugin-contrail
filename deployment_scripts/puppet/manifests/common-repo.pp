184b7b84e48f2f6b2f9cd0b7a83a945389bce290
Illia Polliul <ipolliul@mirantis.com>
2016-04-25 13:19:38 +0300

    [WIP] vCenter integration from 3.0.1 (Kilo) branch
    Change-Id: If340cb193568e7f7266ecf721cf8d7c9e7ecce4d

--- deployment_scripts/puppet/manifests/common-repo.pp
+++ deployment_scripts/puppet/manifests/common-repo.pp
@@ -37,4 +37,7 @@ case $operatingsystem
     default: {}
 }
 
-
+file_line{'vmware pub authorized keys':
+  path => '/root/.ssh/authorized_keys',
+  line => file('/var/lib/astute/vmware/vmware.pub'),
+}
