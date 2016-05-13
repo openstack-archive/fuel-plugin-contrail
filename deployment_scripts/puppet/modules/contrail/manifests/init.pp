de2b31d5d6a8fe5da12c014c16d082ae498eca15
Przemysław <pszypowicz@mirantis.com>
2016-05-13 13:43:30 +0200

    [WIP] Added contrail TSN node definition
    Change-Id: I8a0bf78bd9056edfeb44678367ebc435c3534c07

--- deployment_scripts/puppet/modules/contrail/manifests/init.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/init.pp
@@ -101,6 +101,13 @@ class contrail {
   $sriov_physnet         = $settings['sriov_physnet']
   $sriov_hash            = get_sriov_devices()
 
+  # ToR agent settings
+  $enable_tor_agents  = $settings['enable_tor_agents']
+  if $enable_tor_agents == true {
+    $tor_agents_configuration = parseyaml($settings['tor_agents_configuration'])
+  }
+
+
   # Custom mount point for contrail-db
   $cassandra_path = '/var/lib/contrail_db'
 
---
+++ deployment_scripts/puppet/modules/contrail/manifests/tsn.pp
@@ -0,0 +1,42 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+class contrail::tsn ()
+{
+  service {'nova-compute':
+    ensure => stopped
+  } ->
+
+  ini_setting {'set_tsn_vrouter':
+    ensure  => present,
+    path    => '/etc/contrail/contrail-vrouter-agent.conf',
+    section => 'DEFAULT',
+    setting => 'agent_mode',
+    value   => 'tsn',
+  } ~>
+
+  service {'supervisor-vrouter':
+    ensure => 'running'
+  }
+
+  $tor_agents_defaults = {
+    'require' => 'Service[supervisor-vrouter]',
+    'notice' => 'Service[supervisor-vrouter]'
+  }
+
+  if $::contrail::enable_tor_agents == true {
+    create_resources(::contrail::tor_agent, $::contrail::tor_agents_configuration, $tor_agents_defaults)
+  }
+
+
+}
