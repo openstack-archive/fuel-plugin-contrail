62fb7785a0518e4befc68b34a80d527fd5a07ba8
Oleksandr Martsyniuk <omartsyniuk@mirantis.com>
2016-05-13 18:35:00 +0300

    Package pin for libnl-route added
    Change-Id: Ie72f5eb1b96fea6be24fd2266cec208e8041c9fe
    Closes-bug: 1581574

--- deployment_scripts/puppet/manifests/contrail-compute-vmware-repo.pp
+++ deployment_scripts/puppet/manifests/contrail-compute-vmware-repo.pp
@@ -12,7 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-notice('MODULAR: contrail/contrail-compute-repo.pp')
+notice('MODULAR: contrail/contrail-compute-vmware-repo.pp')
 
 include contrail
 class { 'contrail::compute::vmware_repo': }
