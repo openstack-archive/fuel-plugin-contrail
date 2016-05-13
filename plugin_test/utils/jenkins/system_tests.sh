5c558c8932aa1f7e34d753d6d292e32e7703853c
Ruslan Khozinov <rkhozinov@mirantis.com>
2016-05-13 18:42:26 +0300

    The script for testrun was updated
     - vcenter support was added
     - refactoring
    
    Change-Id: I34f5b8b6cc5f35511b24886e2b72486e37d7de56

--- plugin_test/utils/jenkins/system_tests.sh
+++ plugin_test/utils/jenkins/system_tests.sh
@@ -1,27 +1,11 @@
 #!/bin/sh
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 
-# functions
-
-INVALIDOPTS_ERR=100
-NOJOBNAME_ERR=101
-NOISOPATH_ERR=102
-NOTASKNAME_ERR=103
-NOWORKSPACE_ERR=104
-DEEPCLEAN_ERR=105
-MAKEISO_ERR=106
-NOISOFOUND_ERR=107
-COPYISO_ERR=108
-SYMLINKISO_ERR=109
-CDWORKSPACE_ERR=110
-ISODOWNLOAD_ERR=111
-INVALIDTASK_ERR=112
-
 # Defaults
-
 export REBOOT_TIMEOUT=${REBOOT_TIMEOUT:-5000}
 export ALWAYS_CREATE_DIAGNOSTIC_SNAPSHOT=${ALWAYS_CREATE_DIAGNOSTIC_SNAPSHOT:-true}
 
+<<<<<<< HEAD
 # Export specified settings
 if [ -z "$NODE_VOLUME_SIZE" ]; then export NODE_VOLUME_SIZE=350; fi
 if [ -z "$OPENSTACK_RELEASE" ]; then export OPENSTACK_RELEASE=Ubuntu; fi
@@ -33,11 +17,12 @@ if [ -z "$SLAVE_NODE_CPU" ]; then export SLAVE_NODE_CPU=4; fi
 
 # Init and update submodule
 git submodule update --init --recursive --remote
+=======
+>>>>>>> 95f59e6... The script for test run was updated
 
 sudo /sbin/iptables -F
 sudo /sbin/iptables -t nat -F
 sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-
 
 ShowHelp() {
 cat << EOF
@@ -57,25 +42,14 @@ if you do need to override them.
 -V (dir)    - Path to python virtual environment
 -i (file)   - Full path to ISO file to build or use for tests.
               Made from iso dir and name if not set.
--t (name)   - Name of task this script should perform. Should be one of defined ones.
-              Taken from Jenkins' job's suffix if not set.
 -o (str)    - Allows you any extra command line option to run test job if you
               want to use some parameters.
 -a (str)    - Allows you to path NOSE_ATTR to the test job if you want
               to use some parameters.
 -A (str)    - Allows you to path  NOSE_EVAL_ATTR if you want to enter attributes
               as python expressions.
--m (name)   - Use this mirror to build ISO from.
-              Uses 'srt' if not set.
--U          - ISO URL for tests.
-              Null by default.
--r (yes/no) - Should built ISO file be places with build number tag and
-              symlinked to the last build or just copied over the last file.
--b (num)    - Allows you to override Jenkins' build number if you need to.
 -l (dir)    - Path to logs directory. Can be set by LOGS_DIR evironment variable.
               Uses WORKSPACE/logs if not set.
--d          - Dry run mode. Only show what would be done and do nothing.
-              Useful for debugging.
 -k          - Keep previously created test environment before tests run
 -K          - Keep test environment after tests are finished
 -h          - Show this help page
@@ -86,72 +60,11 @@ by exported variable before script is run or by one of command line options.
 You can override following variables using export VARNAME="value" before running this script
 WORKSPACE  - path to directory where Fuelweb repository was checked out by Jenkins or manually
 JOB_NAME   - name of Jenkins job that determines which task should be done and ISO file name.
-
-If task name is "iso" it will make iso file
-Other defined names will run Nose tests using previously built ISO file.
-
-ISO file name is taken from job name prefix
-Task name is taken from job name suffix
-Separator is one dot '.'
-
-For example if JOB_NAME is:
-mytest.somestring.iso
-ISO name: mytest.iso
-Task name: iso
-If ran with such JOB_NAME iso file with name mytest.iso will be created
-
-If JOB_NAME is:
-mytest.somestring.node
-ISO name: mytest.iso
-Task name: node
-If script was run with this JOB_NAME node tests will be using ISO file mytest.iso.
-
-First you should run mytest.somestring.iso job to create mytest.iso.
-Then you can ran mytest.somestring.node job to start tests using mytest.iso and other tests too.
 EOF
 }
 
-GlobalVariables() {
-  # where built iso's should be placed
-  # use hardcoded default if not set before by export
-  ISO_DIR="${ISO_DIR:=/var/www/fuelweb-iso}"
-
-  # name of iso file
-  # taken from jenkins job prefix
-  # if not set before by variable export
-  if [ -z "${ISO_NAME}" ]; then
-    ISO_NAME="${JOB_NAME%.*}.iso"
-  fi
-
-  # full path where iso file should be placed
-  # make from iso name and path to iso shared directory
-  # if was not overriden by options or export
-  if [ -z "${ISO_PATH}" ]; then
-    ISO_PATH="${ISO_DIR}/${ISO_NAME}"
-  fi
-
-  # what task should be ran
-  # it's taken from jenkins job name suffix if not set by options
-  if [ -z "${TASK_NAME}" ]; then
-    TASK_NAME="${JOB_NAME##*.}"
-  fi
-
-  # do we want to keep iso's for each build or just copy over single file
-  ROTATE_ISO="${ROTATE_ISO:=yes}"
-
-  # choose mirror to build iso from. Default is 'srt' for Saratov's mirror
-  # you can change mirror by exporting USE_MIRROR variable before running this script
-  USE_MIRROR="${USE_MIRROR:=srt}"
-
-  # only show what commands would be executed but do nothing
-  # this feature is usefull if you want to debug this script's behaviour
-  DRY_RUN="${DRY_RUN:=no}"
-
-  VENV="${VENV:=yes}"
-}
-
 GetoptsVariables() {
-  while getopts ":w:j:i:t:o:a:A:m:U:r:b:V:l:dkKe:v:h" opt; do
+  while getopts ":w:j:i:o:a:A:V:l:kKe:v:h" opt; do
     case $opt in
       w)
         WORKSPACE="${OPTARG}"
@@ -162,9 +75,6 @@ GetoptsVariables() {
       i)
         ISO_PATH="${OPTARG}"
         ;;
-      t)
-        TASK_NAME="${OPTARG}"
-        ;;
       o)
         TEST_OPTIONS="${TEST_OPTIONS} ${OPTARG}"
         ;;
@@ -173,18 +83,6 @@ GetoptsVariables() {
         ;;
       A)
         NOSE_EVAL_ATTR="${OPTARG}"
-        ;;
-      m)
-        USE_MIRROR="${OPTARG}"
-        ;;
-      U)
-        ISO_URL="${OPTARG}"
-        ;;
-      r)
-        ROTATE_ISO="${OPTARG}"
-        ;;
-      b)
-        BUILD_NUMBER="${OPTARG}"
         ;;
       V)
         VENV_PATH="${OPTARG}"
@@ -200,9 +98,6 @@ GetoptsVariables() {
         ;;
       e)
         ENV_NAME="${OPTARG}"
-        ;;
-      d)
-        DRY_RUN="yes"
         ;;
       v)
         VENV="no"
@@ -227,57 +122,75 @@ GetoptsVariables() {
 
 CheckVariables() {
 
-  if [ -z "${JOB_NAME}" ]; then
-    echo "Error! JOB_NAME is not set!"
-    exit $NOJOBNAME_ERR
-  fi
+  # Common settings
+  [ -z "${JOB_NAME}"  ] && { echo "Error! JOB_NAME is not set!" ; exit 1;  }
+  [ -z "${ISO_PATH}"  ] && { echo "Error! ISO_PATH is not set!" ; exit 1;  }
+  [ -z "${ENV_NAME}"  ] && { echo "Error! ENV_NAME is not set!" ; exit 1;  }
+  [ -z "${WORKSPACE}" ] && { echo "Error! WORKSPACE is not set!"; exit 1;  }
+  [ -z "${VENV_PATH}" ] && { echo "Error! virtualenv is not set!"; exit 1; }
+  [ -z "${LOGS_DIR}"  ] && LOGS_DIR="${WORKSPACE}/logs" 
 
-  if [ -z "${ISO_PATH}" ]; then
-    echo "Error! ISO_PATH is not set!"
-    exit $NOISOPATH_ERR
-  fi
+  # Fuel envronment settings
+  [ -z $NODE_VOLUME_SIZE  ] &&  export NODE_VOLUME_SIZE=350
+  [ -z $OPENSTACK_RELEASE ] &&  export OPENSTACK_RELEASE=Ubuntu
+  [ -z $ENV_NAME          ] &&  export ENV_NAME="contrail"
+  [ -z $ADMIN_NODE_MEMORY ] &&  export ADMIN_NODE_MEMORY=4096
+  [ -z $ADMIN_NODE_CPU    ] &&  export ADMIN_NODE_CPU=4
+  [ -z $SLAVE_NODE_MEMORY ] &&  export SLAVE_NODE_MEMORY=4096
+  [ -z $SLAVE_NODE_CPU    ] &&  export SLAVE_NODE_CPU=4
+  
+  # vSphere settings
+  [ -z "${VCENTER_USERNAME}"   ] && { echo "Error! VCENTER_USERNAME is not set!"; exit 1; }
+  [ -z "${VCENTER_PASSWORD}"   ] && { echo "Error! VCENTER_PASSWORD is not set!"; exit 1; }
+  [ -z "${VCENTER_USE}"        ] && export VCENTER_USE="true"  
+  [ -z "${VCENTER_IP}"         ] && export VCENTER_IP="172.16.0.254"
+  [ -z "${VCENTER_DATACENTER}" ] && export VC_DATACENTER="Datacenter"
+  [ -z "${VCENTER_DATASTORE}"  ] && export VC_DATASTORE="nfs"
+  [ -z "${VCENTER_CLUSTERS}"   ] && export VCENTER_CLUSTERS="Cluster1,Cluster2"
 
-  if [ -z "${TASK_NAME}" ]; then
-    echo "Error! TASK_NAME is not set!"
-    exit $NOTASKNAME_ERR
-  fi
+  # Workstation settings
+  [ -z "${WORKSTATION_USERNAME}" ] && { echo "Error! WORKSTATION_USERNAME is not set!"; exit 1; }
+  [ -z "${WORKSTATION_PASSWORD}" ] && { echo "Error! WORKSTATION_PASSWORD is not set!"; exit 1; }
+  [ -z "${WORKSTATION_NODES}"    ] && export WORKSTATION_NODES="esxi1 esxi2 esxi3 vcenter trusty"
+  [ -z "${WORKSTATION_IFS}"      ] && export WORKSTATION_IFS="vmnet1 vmnet2"
+  [ -z "${WORKSTATION_SNAPSHOT}" ] && export WORKSTATION_SNAPSHOT="contrail"
+  [ -z "${WORKSTATION_TIMEOUT}"  ] && export WORKSTATION_TIMEOUT=1 
 
-  if [ -z "${WORKSPACE}" ]; then
-    echo "Error! WORKSPACE is not set!"
-    exit $NOWORKSPACE_ERR
-  fi
+  [ -z "${WORKSTATION_PUBLIC_GATEWAY}" ] && export WORKSTATION_PUBLIC_GATEWAY="172.16.0.1/24"
+  [ -z "${NET_CREATION_TIMEOUT}" ] && export NET_CREATION_TIMEOUT=5
+  
 }
 
-MakeISO() {
-  # Create iso file to be used in tests
+RunTest() {
+    [ ! -f "${LOGS_DIR}"  ] &&  mkdir -p $LOGS_DIR
 
-  # clean previous garbage
-  if [ "${DRY_RUN}" = "yes" ]; then
-    echo make deep_clean
-  else
-    make deep_clean
-  fi
-  ec="${?}"
+    [ "${VENV}" == "yes" ] && . $VENV_PATH/bin/activate
 
-  if [ "${ec}" -gt "0" ]; then
-    echo "Error! Deep clean failed!"
-    exit $DEEPCLEAN_ERR
-  fi
+    if [ "${KEEP_BEFORE}" != "yes" ]; then
+      [ $(dos.py list | grep "^${ENV_NAME}\$") ] &&  dos.py erase "${ENV_NAME}"
+    fi
 
-  # create ISO file
-  export USE_MIRROR
-  if [ "${DRY_RUN}" = "yes" ]; then
-    echo make iso
-  else
-    make iso
-  fi
-  ec=$?
+    # gather additional option for this nose test run
+    OPTS=""
+    if [ -n "${NOSE_ATTR}" ]; then
+        OPTS="${OPTS} -a ${NOSE_ATTR}"
+    fi
+    if [ -n "${NOSE_EVAL_ATTR}" ]; then
+        OPTS="${OPTS} -A ${NOSE_EVAL_ATTR}"
+    fi
+    if [ -n "${TEST_OPTIONS}" ]; then
+        OPTS="${OPTS} ${TEST_OPTIONS}"
+    fi
 
-  if [ "${ec}" -gt "0" ]; then
-    echo "Error making ISO!"
-    exit $MAKEISO_ERR
-  fi
+    clean_old_bridges
 
+    export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${WORKSPACE}"
+    echo ${PYTHONPATH}
+    python plugin_test/run_tests.py -q --nologcapture --with-xunit ${OPTS} &
+
+    SYSTEST_PID=$!
+
+<<<<<<< HEAD
   if [ "${DRY_RUN}" = "yes" ]; then
     ISO="${WORKSPACE}/build/iso/fuel.iso"
   else
@@ -286,79 +199,48 @@ MakeISO() {
     if [ ! -f "${ISO}" ]; then
       echo "Error! ISO file not found!"
       exit $NOISOFOUND_ERR
-    fi
-  fi
-
-  # copy ISO file to storage dir
-  # if rotation is enabled and build number is aviable
-  # save iso to tagged file and symlink to the last build
-  # if rotation is not enabled just copy iso to iso_dir
-
-  if [ "${ROTATE_ISO}" = "yes" -a "${BUILD_NUMBER}" != "" ]; then
-    # copy iso file to shared dir with revision tagged name
-    NEW_BUILD_ISO_PATH="${ISO_PATH#.iso}_${BUILD_NUMBER}.iso"
-    if [ "${DRY_RUN}" = "yes" ]; then
-      echo cp "${ISO}" "${NEW_BUILD_ISO_PATH}"
-    else
-      cp "${ISO}" "${NEW_BUILD_ISO_PATH}"
-    fi
-    ec=$?
-
-    if [ "${ec}" -gt "0" ]; then
-      echo "Error! Copy ${ISO} to ${NEW_BUILD_ISO_PATH} failed!"
-      exit $COPYISO_ERR
+=======
+    if ! ps -p $SYSTEST_PID > /dev/null
+    then
+      echo "System tests exited prematurely, aborting"
+      exit 1
+>>>>>>> 95f59e6... The script for test run was updated
     fi
 
-    # create symlink to the last built ISO file
-    if [ "${DRY_RUN}" = "yes" ]; then
-      echo ln -sf "${NEW_BUILD_ISO_PATH}" "${ISO_PATH}"
-    else
-      ln -sf "${NEW_BUILD_ISO_PATH}" "${ISO_PATH}"
-    fi
-    ec=$?
+    # Waiting for fuel-env's networks creation
+    while [ $(virsh net-list | grep $ENV_NAME | wc -l) -ne 5 ]; do
+      sleep $NET_CREATION_TIMEOUT
+      if ! ps -p $SYSTEST_PID > /dev/null
+      then
+        echo System tests exited prematurely, aborting
+        exit 1
+      fi
+    done
+    sleep $NET_CREATION_TIMEOUT
 
-    if [ "${ec}" -gt "0" ]; then
-      echo "Error! Create symlink from ${NEW_BUILD_ISO_PATH} to ${ISO_PATH} failed!"
-      exit $SYMLINKISO_ERR
-    fi
-  else
-    # just copy file to shared dir
-    if [ "${DRY_RUN}" = "yes" ]; then
-      echo cp "${ISO}" "${ISO_PATH}"
-    else
-      cp "${ISO}" "${ISO_PATH}"
-    fi
-    ec=$?
+    # Configre vcenter nodes and interfaces
+    setup_net $ENV_NAME
+    clean_iptables
+    revert_ws "$WORKSTATION_NODES" || { echo "killing $SYSTEST_PID and its childs" && pkill --parent $SYSTEST_PID && kill $SYSTEST_PID && exit 1; }
 
-    if [ "${ec}" -gt "0" ]; then
-      echo "Error! Copy ${ISO} to ${ISO_PATH} failed!"
-      exit $COPYISO_ERR
-    fi
-  fi
+    #fixme should use only one clean_iptables call
+    clean_iptables
 
-  if [ "${ec}" -gt "0" ]; then
-    echo "Error! Copy ISO from ${ISO} to ${ISO_PATH} failed!"
-    exit $COPYISO_ERR
-  fi
-  echo "Finished building ISO: ${ISO_PATH}"
-  exit 0
+    echo waiting for system tests to finish
+    wait $SYSTEST_PID
+
+    export RES=$?
+    echo ENVIRONMENT NAME is $ENV_NAME
+    virsh net-dumpxml ${ENV_NAME}_admin | grep -P "(\d+\.){3}" -o | awk '{print "Fuel master node IP: "$0"2"}'
+
+    echo Result is $RES
+
+    [ "${KEEP_AFTER}" != "yes" ] && dos.py destroy "${ENV_NAME}"
+
+    exit "${RES}"
 }
 
-CdWorkSpace() {
-    # chdir into workspace or fail if could not
-    if [ "${DRY_RUN}" != "yes" ]; then
-        cd "${WORKSPACE}"
-        ec=$?
-
-        if [ "${ec}" -gt "0" ]; then
-            echo "Error! Cannot cd to WORKSPACE!"
-            exit $CDWORKSPACE_ERR
-        fi
-    else
-        echo cd "${WORKSPACE}"
-    fi
-}
-
+<<<<<<< HEAD
 RunTest() {
     # Run test selected by task name
 
@@ -381,36 +263,77 @@ RunTest() {
             fi
         fi
     fi
+=======
+#Define functions for VCenter configuration
+add_interface_to_bridge() {
+  env=$1
+  net_name=$2
+  nic=$3
+  ip=$4
+>>>>>>> 95f59e6... The script for test run was updated
 
-    if [ -z "${VENV_PATH}" ]; then
-        VENV_PATH="/home/jenkins/venv-nailgun-tests"
-    fi
+  for net in $(virsh net-list | grep ${env}_${net_name} | awk '{print $1}'); do
+    bridge=$(virsh net-info $net | grep -i bridge | awk '{print $2}')
+    setup_bridge $bridge $nic $ip && echo $net_name bridge $bridge ready
+  done
+}
 
-    # run python virtualenv
-    if [ "${VENV}" = "yes" ]; then
-        if [ "${DRY_RUN}" = "yes" ]; then
-            echo . $VENV_PATH/bin/activate
-        else
-            . $VENV_PATH/bin/activate
-        fi
-    fi
+setup_bridge() {
+  bridge=$1
+  nic=$2
+  ip=$3
 
-    if [ "${ENV_NAME}" = "" ]; then
-      ENV_NAME="${JOB_NAME}_system_test"
-    fi
+  sudo /sbin/brctl stp $bridge off
+  sudo /sbin/brctl addif $bridge $nic
+  if [ ! -z "${ip}" ]; then
+    #set if with existing ip down
+    for itf in $(sudo ip -o addr show to $ip | cut -d' ' -f2); do
+        echo deleting $ip from $itf
+        sudo ip addr del dev $itf $ip
+    done
+    echo "adding $ip to $bridge"
+    sudo /sbin/ip addr add $ip dev $bridge
+    echo "$nic added to $bridge"
+  fi
 
-    if [ "${LOGS_DIR}" = "" ]; then
-      LOGS_DIR="${WORKSPACE}/logs"
-    fi
+  sudo /sbin/ip link set dev $bridge up
+  if sudo /sbin/iptables-save |grep $bridge | grep -i reject| grep -q FORWARD;then
+    sudo /sbin/iptables -D FORWARD -o $bridge -j REJECT --reject-with icmp-port-unreachable
+    sudo /sbin/iptables -D FORWARD -i $bridge -j REJECT --reject-with icmp-port-unreachable
+  fi
+}
 
+clean_old_bridges() {
+  bridge_info=$(/sbin/brctl show | grep -v "bridge name" | cut -f1 -d'	')
+  for intf in $WORKSTATION_IFS; do
+    for br in $bridge_info ; do
+      /sbin/brctl show $br| grep -q $intf && sudo /sbin/brctl delif $br $intf \
+        && sudo /sbin/ip link set dev $br down && echo $intf deleted from $br
+    done
+  done
+}
+
+<<<<<<< HEAD
     if [ ! -f "$LOGS_DIR" ]; then
       mkdir -p "$LOGS_DIR"
     fi
+=======
+clean_iptables() {
+  sudo /sbin/iptables -F
+  sudo /sbin/iptables -t nat -F
+  sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+}
+>>>>>>> 95f59e6... The script for test run was updated
 
-    export ENV_NAME
-    export LOGS_DIR
-    export ISO_PATH
+# waiting for ending of parallel processes
+wait() {
+  while [ $(pgrep vmrun | wc -l) -ne 0 ] ; do
+    [[ "${DEBUG}" == "true" ]] && echo $(pgrep vmrun)
+    sleep $WORKSTATION_TIMEOUT
+  done
+}
 
+<<<<<<< HEAD
     if [ "${KEEP_BEFORE}" != "yes" ]; then
       # remove previous environment
       if [ "${DRY_RUN}" = "yes" ]; then
@@ -421,19 +344,21 @@ RunTest() {
         fi
       fi
     fi
+=======
+revert_ws() {
+  nodes=$1
+  snapshot=$WORKSTATION_SNAPSHOT
+  cmd="vmrun -T ws-shared -h https://localhost:443/sdk -u ${WORKSTATION_USERNAME} -p ${WORKSTATION_PASSWORD}"
+>>>>>>> 95f59e6... The script for test run was updated
 
-    # gather additional option for this nose test run
-    OPTS=""
-    if [ -n "${NOSE_ATTR}" ]; then
-        OPTS="${OPTS} -a ${NOSE_ATTR}"
-    fi
-    if [ -n "${NOSE_EVAL_ATTR}" ]; then
-        OPTS="${OPTS} -A ${NOSE_EVAL_ATTR}"
-    fi
-    if [ -n "${TEST_OPTIONS}" ]; then
-        OPTS="${OPTS} ${TEST_OPTIONS}"
-    fi
+  # reverting in parallel
+  for node in $nodes; do
+    echo "Reverting $node to $snapshot"
+    $cmd revertToSnapshot "[standard] $node/$node.vmx" $snapshot || \
+      { echo "Error: reverting of $node has failed";  exit 1; } &
+  done
 
+<<<<<<< HEAD
     # run python test set to create environments, deploy and test product
     if [ "${DRY_RUN}" = "yes" ]; then
         echo export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${WORKSPACE}"
@@ -442,55 +367,32 @@ RunTest() {
         export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${WORKSPACE}"
         echo "${PYTHONPATH}"
         python plugin_test/run_tests.py -q --nologcapture --with-xunit ${OPTS}
+=======
+  wait
+>>>>>>> 95f59e6... The script for test run was updated
 
-    fi
-    ec=$?
+  for node in $nodes; do
+    echo "Starting $node"
+    $cmd start "[standard] $node/$node.vmx" ||\
+      echo "Error: $node failed to start" &
+  done
 
-    if [ "${KEEP_AFTER}" != "yes" ]; then
-      # remove environment after tests
-      if [ "${DRY_RUN}" = "yes" ]; then
-        echo dos.py destroy "${ENV_NAME}"
-      else
-        dos.py destroy "${ENV_NAME}"
-      fi
-    fi
-
-    exit "${ec}"
+  wait
 }
 
-RouteTasks() {
-  # this selector defines task names that are recognised by this script
-  # and runs corresponding jobs for them
-  # running any jobs should exit this script
-
-  case "${TASK_NAME}" in
-  test)
-    RunTest
-    ;;
-  iso)
-    MakeISO
-    ;;
-  *)
-    echo "Unknown task: ${TASK_NAME}!"
-    exit $INVALIDTASK_ERR
-    ;;
-  esac
-  exit 0
+setup_net() {
+  env=$1
+  add_interface_to_bridge $env private vmnet2
+  add_interface_to_bridge $env public  vmnet1 $WORKSTATION_PUBLIC_GATEWAY 
 }
-
-# MAIN
 
 # first we want to get variable from command line options
 GetoptsVariables "${@}"
 
-# then we define global variables and there defaults when needed
-GlobalVariables
-
 # check do we have all critical variables set
 CheckVariables
 
-# first we chdir into our working directory unless we dry run
-CdWorkSpace
+[ -f ${WORKSPACE} ] && cd ${WORKSPACE} || { echo "${WORKSPACE} is not defined or doesn't exist"; exit 1; }
 
-# finally we can choose what to do according to TASK_NAME
-RouteTasks
+RunTest
+
