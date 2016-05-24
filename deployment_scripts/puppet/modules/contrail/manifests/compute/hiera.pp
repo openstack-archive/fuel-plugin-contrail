ff9881e34849a716c34e6aafaceedbadbecc7bf5
Alexander Noskov <anoskov@mirantis.com>
2016-05-24 10:48:46 -0500

    Wrap tasks into includeable classes
    In terms of supporting plugin tasks in puppet-master approach we need to
    wrap each task into puppet classes.
    Minor puppet style fixes
    
    Change-Id: I6051d199008ce70f7225ddcea98a6702c2da1dec

--- deployment_scripts/puppet/modules/contrail/manifests/compute/hiera.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/compute/hiera.pp
@@ -1,4 +1,4 @@
-#    Copyright 2016 Mirantis, Inc.
+#    Copyright 2015 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -12,6 +12,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-notice('MODULAR: contrail/common-repo.pp')
+class contrail::compute::hiera {
 
-class { 'contrail::common_repo': }
+  # prevent to install and run open vSwitch on compute nodes
+  file { '/etc/hiera/plugins/contrail.yaml':
+    ensure  => file,
+    content => 'use_ovs: false',
+  }
+}
