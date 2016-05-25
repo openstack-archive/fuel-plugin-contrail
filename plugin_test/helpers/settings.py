f53050c2cd5d87f4cfed54bf6262bd75008a24aa
Denis Deryabin <dderyabin@mirantis.com>
2016-05-25 16:01:38 +0300

    Add DPDK tests
    Change-Id: Ib552a22c6403b265de079eac9421edb1bbd4a06b

--- plugin_test/helpers/settings.py
+++ plugin_test/helpers/settings.py
@@ -21,9 +21,16 @@ VSRX_TEMPLATE_PATH = os.environ.get('VSRX_TEMPLATE_PATH', False)
 OSFT_RUN_TIMEOUT = 45 * 60  # 45 minutes
 DEPLOY_CLUSTER_TIMEOUT = 25 * 60  # 25 minutes
 
-BAREMETAL_IMPI = {
-    'user': os.environ.get('BM_IPMI_USER'),
-    'password': os.environ.get('BM_IPMI_PASSWORD'),
-    'host': os.environ.get('BM_IPMI_HOST'),
-    'remote_ip': os.environ.get('BM_IP'),
+BAREMETAL = {
+    # Target Baremetal host
+    'ipmi_user': os.environ.get('BM_IPMI_USER'),
+    'ipmi_password': os.environ.get('BM_IPMI_PASSWORD'),
+    'ipmi_host': os.environ.get('BM_IPMI_ADDR'),
+
+    'target_mac': os.environ.get('BM_TARGET_MAC'),
+
+    # Host where tests are running
+    'host_ip': os.environ.get('BM_HOST_IP'),
+    'host_bridge_interfaces': os.environ.get('BM_HOST_BRIDGE_INTERFACES'),
+    'host_fuel_interfaces': os.environ.get('BM_HOST_FUEL_INTERFACES')
 }
