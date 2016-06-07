c7e1ac8afdfe5894fd2dab0b63d659b63bb2f1b2
Andrey Kirilochkin <akirilochkin@mirantis.com>
2016-06-07 18:03:57 -0500

    vCenter integration for Fuel 9.0kilo
    These changes allow to deploy contrail with vCenter integration:
     - Allows to deploy ContrailVM on each ESXi node
     - Allows to deploy nova-compute with vCenter configuration
     - Allows to deploy Openstack with vCenter
     - Unlocks vCenter tab in FUEL-UI
     - Added contrail-vmware role for vRouter
     - Added functionality to deploy(node have to be created by hands)
     vRouter by FUEL on ESXi
    
    Change-Id: I1a01f3aecd459f6b837ffdb9f4c59883b5df8535

---
+++ deployment_scripts/puppet/modules/contrail/files/additional_tasks.py
@@ -0,0 +1,81 @@
+import time
+from fabfile.utils.fabos import *
+from fabric.contrib.files import exists
+
+from fabfile.tasks.install import install_pkg_node
+from fabfile.tasks.install import create_install_repo_node
+from fabfile.tasks.install import apt_install
+
+
+@task
+#@parallel(pool_size=20)
+#use compute role because in testbed we will set conly ContrailVM ips
+@roles('compute')
+def prepare_contrailvm(pkg):
+    """Install local repository of contrail packages on ContrailVM"""
+    time.sleep(30)
+    with settings(password='c0ntrail123', connection_attempts=10):
+        sudo('echo > /etc/apt/sources.list')
+        execute(install_pkg_node, pkg, env.host_string)
+        execute(create_install_repo_node, env.host_string)
+
+
+@task
+#@parallel(pool_size=20)
+#use compute role because in testbed we will set conly ContrailVM ips
+@roles('compute')
+def fab_install_vrouter():
+    """Install vrouter packages on ContrailVM"""
+    with settings(password='c0ntrail123', connection_attempts=10):
+        vrouter_pkg = ['contrail-vrouter-dkms', 'contrail-vrouter-common', 'contrail-nova-vif']
+        apt_install(vrouter_pkg)
+
+
+@task
+@parallel(pool_size=20)
+def disable_add_vnc_config():
+    """Disable provision vrouter when exucute setup-vnc-compute on ContrailVM.
+       On ContrailVMs we don't have access to managment network, so we don't
+       have access to keystone."""
+    with settings(password='c0ntrail123', connection_attempts=20):
+        patched_file = '/usr/local/lib/python2.7/dist-packages/contrail_provisioning/compute/common.py'
+        cmd = 'sed -i "s~python\ /opt/contrail/utils/provision_vrouter\.py~echo~g" %s' % patched_file
+        sudo(cmd)
+
+
+@task
+def provision_contrailvm(cmd):
+    """Create configuration files on ContrailVM"""
+    with settings(password='c0ntrail123', connection_attempts=10):
+        sudo(cmd)
+
+@task
+def deploy_agent_param():
+    """Deploy agent_param.tmpl on ContrailVM"""
+    with settings(password='c0ntrail123', connection_attempts=10):
+        cmd = '/opt/contrail/bin/vnagent_param_setup.sh'
+        sudo(cmd)
+
+@task
+def change_hostname(hostname):
+    """Change hostname on ContrailVM"""
+    with settings(password='c0ntrail123', connection_attempts=10):
+        cmd = 'echo %s > /etc/hostname' % hostname
+        sudo(cmd)
+
+
+@task
+def set_ntp(ntp):
+    """Set proper ntp server on ContrailVM"""
+    with settings(password='c0ntrail123', connection_attempts=10):
+        sudo('sed -i "/^server/g" /etc/ntp.conf')
+        sudo('echo "server %s" >> /etc/ntp.conf' % ntp)
+
+@task
+def redeploy_vrouter_module():
+    """Deploy agent_param.tmpl on ContrailVM"""
+    with settings(password='c0ntrail123', connection_attempts=10):
+        cmd = 'dkms uninstall vrouter/3.0.2.0'
+        cmd1 = 'dpkg -i /opt/contrail/contrail_install_repo/contrail-vrouter-dkms*'
+        sudo(cmd)
+        sudo(cmd1)
---
+++ deployment_scripts/puppet/modules/contrail/manifests/contrail_vmware.pp
@@ -0,0 +1,169 @@
+#    Copyright 2016 Mirantis, Inc.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+class contrail::contrail_vmware {
+if $contrail::use_vcenter and $contrail::provision_vmware_type == 'fuel'{
+
+    $self_ip = $contrail::address
+    $cfgm_ip = $contrail::contrail_private_vip
+
+    $ncontrols = size($contrail::contrail_control_ips)
+    $amqp_server_ip = $contrail::contrail_private_vip
+    $service_token = $contrail::admin_token
+    $orchestrator = $contrail::orchestrator
+    $hypervisor = $contrail::hypervisor
+    $keystone_ip = $contrail::mos_mgmt_vip
+    $keystone_admin_user = $contrail::neutron_user
+    $keystone_admin_password = $contrail::service_token
+    $service_tenant_name = $contrail::service_tenant
+    $internal_vip = $contrail::mos_mgmt_vip
+    $external_vip = $contrail::mos_public_vip
+    $contrail_internal_vip = $contrail::contrail_private_vip
+    $mgmt_self_ip = $::ipaddress_br_mgmt
+
+    # Fetching the esxi data from hash
+    $esxi_data = fetch_esxi_data("root@${self_ip}")
+    $vmware = $esxi_data['ip']
+    $vmware_username = $esxi_data['username']
+    $vmware_passwd = $esxi_data['password']
+    $vmware_iface_name = $esxi_data['contrail_vm']['vmware_iface_name']
+    $vmware_vmpg_vswitch = $esxi_data['fabric_vswitch']
+    $vmware_vmpg_vswitch_mtu = '9000'
+    $mode = $contrail::mode
+    $contrailvm_ntp = $contrail::contrailvm_ntp
+
+    $phys_dev_facter = regsubst($::contrail::phys_dev, '\.' , '_')
+    $dev_mac         = getvar("::macaddress_${phys_dev_facter}")
+
+    $delete_packages  = ['openvswitch-common', 'openvswitch-datapath-dkms', 'openvswitch-datapath-lts-saucy-dkms', 'openvswitch-switch', 'nova-network', 'nova-api']
+    $install_packages = ['contrail-install-packages', 'contrail-fabric-utils', 'contrail-setup', 'contrail-vrouter-dkms', 'contrail-vrouter-common', 'contrail-nova-vif', 'open-vm-tools', 'iproute2']
+
+    l23network::l3::ifconfig { $vmware_iface_name: ipaddr => 'none' }
+
+    sysctl::value { 'net.nf_conntrack_max': value => '1048576'} ->
+    sysctl::value { 'kernel.panic': value => '60'} ->
+    sysctl::value { 'net.ipv4.conf.default.arp_accept': value => '1'} ->
+    sysctl::value { 'net.core.netdev_max_backlog': value => '261144'} ->
+    sysctl::value { 'net.ipv4.tcp_keepalive_intvl': value => '3'} ->
+    sysctl::value { 'net.ipv4.tcp_keepalive_time': value => '30'} ->
+    sysctl::value { 'net.ipv4.tcp_keepalive_probes': value => '8'} ->
+    sysctl::value { 'net.ipv4.conf.all.arp_accept': value => '1'} ->
+    sysctl::value { 'net.ipv4.tcp_retries2': value => '5'} ->
+    sysctl::value { 'net.ipv4.conf.all.rp_filter': value => '2'} ->
+    sysctl::value { 'net.ipv4.conf.default.rp_filter': value => '2'} ->
+    sysctl::value { 'kernel.core_pattern': value => '/var/crashes/core.%e.%p.%h.%t'} ->
+
+    file {'/var/crashes':
+      mode   => '1777',
+      ensure => directory,
+    } ->
+
+    file_line { 'use_kdump':
+      path  => '/etc/default/kdump-tools',
+      match => 'USE_KDUMP=.*',
+      line  => 'USE_KDUMP=1',
+     } ->
+
+    file_line { 'kexec-tools':
+      path  => '/etc/default/kdump-tools',
+      match => 'crashkernel=.*\([ | \"]\)',
+      line  => 'crashkernel=384M-2G:64M,2G-16G:128M,16G-:256M\1',
+    }
+
+    if !is_pkg_installed('contrail-openstack-vrouter') {
+      file { 'create_supervisor_vrouter_override':
+        ensure  => present,
+        path    => '/etc/init/supervisor-vrouter.override',
+        content => 'manual',
+        before  => Class['contrail::package'],
+      }
+    }
+
+    class { 'contrail::package':
+      install => [$install_packages],
+      remove  => [$delete_packages],
+    } ->
+    exec { 'remove-ovs-modules':
+      command => '/sbin/modprobe -r openvswitch',
+      onlyif  => '/sbin/lsmod | grep -q openvswitch',
+    } ->
+    file {'/etc/contrail/ctrl-details':
+      ensure  => present,
+      content => template('contrail/ctrl-details.erb'),
+      require => Class[Contrail::Package],
+    } ->
+    file {'/etc/contrail/agent_param':
+      ensure  => present,
+      content => template('contrail/agent_param.erb'),
+      require => Class[Contrail::Package],
+    } ->
+    contrail_vrouter_agent {
+      'DEFAULT/platform'                          : value => 'default';
+      'DEFAULT/physical_interface_address'        : value => $contrail::phys_dev_pci;
+      'DEFAULT/physical_interface_mac'            : value => $dev_mac;
+      'DEFAULT/log_file'                          : value => '/var/log/contrail/contrail-vrouter-agent.log';
+      'DEFAULT/log_level'                         : value => 'SYS_NOTICE';
+      'DEFAULT/log_local'                         : value => '1';
+      'DEFUALT/headless_mode'                     : value => 'true';
+      'DISCOVERY/server'                          : value => $contrail::contrail_private_vip;
+      'DISCOVERY/max_control_nodes'               : value => '2';
+      'HYPERVISOR/type'                           : value => 'vmware';
+      'HYPERVISOR/vmware_mode'                    : value => 'vcenter';
+      'HYPERVISOR/vmware_physical_interface'      : value => $vmware_iface_name;
+      'METADATA/metadata_proxy_secret'            : value => $contrail::metadata_secret;
+      'NETWORKS/control_network_ip'               : value => $contrail::address;
+      'VIRTUAL-HOST-INTERFACE/name'               : value => 'vhost0';
+      'VIRTUAL-HOST-INTERFACE/ip'                 : value => "${contrail::address}/${contrail::netmask_short}";
+      'VIRTUAL-HOST-INTERFACE/physical_interface' : value => $contrail::phys_dev;
+      'VIRTUAL-HOST-INTERFACE/gateway'            : value => pick($contrail::gateway, false);
+      'SERVICE-INSTANCE/netns_command'            : value => '/usr/bin/opencontrail-vrouter-netns';
+      'FLOWS/thread_count'                        : value => '2';
+    } ->
+    file {'/etc/contrail/vrouter_nodemgr_param':
+      ensure  => present,
+      content => template('contrail/vrouter_nodemgr_param.erb'),
+    } ->
+    file {'/etc/contrail/contrail-vrouter-nodemgr.conf':
+      ensure  => present,
+      content => template('contrail/contrail-vrouter-nodemgr.conf.erb'),
+    } ->
+    exec { 'remove_supervisor_override':
+      command => '/bin/rm /etc/init/supervisor-vrouter.override',
+      onlyif  => '/usr/bin/test -f /etc/init/supervisor-vrouter.override',
+      require => Class['Contrail::Package'],
+    }
+
+    service {'supervisor-vrouter':
+      ensure    => running,
+      enable    => true,
+      subscribe => [Class[Contrail::Package],Exec['remove-ovs-modules'],
+                    File['/etc/contrail/agent_param',
+                    '/etc/contrail/contrail-vrouter-nodemgr.conf']
+                    ],
+    } ->
+    exec { "register_contrailvm_vrouter":
+      path    => '/usr/local/bin:/bin:/usr/bin/',
+      cwd     => '/opt/contrail/utils',
+      command => "python provision_vrouter.py --host_name ${::fqdn} --host_ip $self_ip --api_server_ip ${contrail_internal_vip} \
+--oper add --admin_user ${keystone_admin_user} --admin_password ${keystone_admin_password} --admin_tenant_name ${service_tenant_name} \
+--openstack_ip ${internal_vip}  && touch /opt/contrail/register_contrailvm_vrouter-DONE",
+      creates => '/opt/contrail/register_contrailvm_vrouter-DONE',
+    }
+    exec {'command when file not exists':
+      command         => "touch /tmp/contrail-reboot-require && touch /opt/contrail/reboot-DONE",
+      onlyif          => "test ! -f /opt/contrail/reboot-DONE",
+      path            => ['/usr/bin','/usr/sbin','/bin','/sbin'],
+    }
+  }
+}
