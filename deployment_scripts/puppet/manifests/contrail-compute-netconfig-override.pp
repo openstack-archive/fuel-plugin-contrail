192fe51003f123d58a3afab4bc5870d76634c766
Pavel Basov <pbasov@mirantis.com>
2016-06-11 17:35:03 -0500

    Override network_scheme to skip interfaces used by DPDK vrouter
      * Depends on new l23network features
      * Overrides br-mesh to vhost0
      * vhost0 pre-up line has to be moved from interfaces.d file
      * Gateway is set if multirack configuration is used
      * Supervisor config is generated in the same way as in virouter init
        script. This prevents unnecessary restarts during task execution
      * Private interface mac is generated by facter instead of a function
      * openstack-network-common-config is skipped for the dpdk role
    
    Change-Id: Ia2eeb5641729261ec4e58f33fe7db69526ffa5a0

--- deployment_scripts/puppet/manifests/contrail-compute-netconfig-override.pp
+++ deployment_scripts/puppet/manifests/contrail-compute-netconfig-override.pp
@@ -1,4 +1,4 @@
-#    Copyright 2015 Mirantis, Inc.
+#    Copyright 2016 Mirantis, Inc.
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -12,19 +12,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-class contrail::compute::hiera {
+notice('MODULAR: contrail/contrail-compute-netconfig-override.pp')
 
-  # prevent to install and run open vSwitch on compute nodes
-  file { '/etc/hiera/plugins/contrail.yaml':
-    ensure  => file,
-    content => "---\nuse_ovs: false\n",
-  }
-
-  if roles_include('dpdk') {
-    file_line {'contrail-vrouter-override_ns':
-        path  => '/etc/hiera.yaml',
-        line  => '    - plugins/contrail-vrouter-override_ns',
-        after => '  !ruby/sym hierarchy:',
-    }
-  }
-}
+include contrail
+class { 'contrail::compute::compute_netconfig_override': }
