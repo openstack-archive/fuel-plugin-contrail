a0aed645084824863af7a9c541ff86c705b7f517
Illia Polliul <ipolliul@mirantis.com>
2016-06-13 19:56:07 +0300

    Fix for gateway value in contrail-vrouter-agent.conf
    Change-Id: I1b27e33c5823b90919b0064fbe8c27f920bbf2f6
    Partial-Bug: 1591405
    Signed-off-by: Illia Polliul <ipolliul@mirantis.com>

--- deployment_scripts/puppet/modules/contrail/manifests/init.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/init.pp
@@ -33,7 +33,13 @@ class contrail {
   # Network configuration
   prepare_network_config($network_scheme)
   $interface         = get_network_role_property('neutron/mesh', 'interface')
-  $gateway           = $network_scheme['endpoints'][$interface]['gateway']
+
+  if $network_scheme['endpoints'][$interface]['routes'] {
+    $gateway = $network_scheme['endpoints'][$interface]['routes'][0]['via']
+  } else {
+    $gateway = false
+  }
+
   $address           = get_network_role_property('neutron/mesh', 'ipaddr')
   $cidr              = get_network_role_property('neutron/mesh', 'cidr')
   $netmask           = get_network_role_property('neutron/mesh', 'netmask')
