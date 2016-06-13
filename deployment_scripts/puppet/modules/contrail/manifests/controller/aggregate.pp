ad54f1b19213dacbae710daa3305520be2608141
Vlad Naboichenko <vnaboychenko@mirantis.com>
2016-06-13 11:24:05 -0500

    Create aggregates fails when using SSL
    hiera('service_endpoint') returns ip instead of fqdn.
    It works for deployments without SSL, but with SSL - fails
    
    Change-Id: I4dffa91f8001c6d2c5d9d8459ddcf97d65cb0797

--- deployment_scripts/puppet/modules/contrail/manifests/controller/aggregate.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/controller/aggregate.pp
@@ -21,6 +21,13 @@ class contrail::controller::aggregate {
     $keystone_user       = pick($nova_hash['user'], 'nova')
     $service_endpoint    = hiera('service_endpoint')
     $region              = hiera('region', 'RegionOne')
+    $ssl_hash               = hiera_hash('use_ssl', {})
+    $internal_auth_protocol = get_ssl_property($ssl_hash, {}, 'keystone', 'internal', 'protocol', 'http')
+    $internal_auth_address  = get_ssl_property($ssl_hash, {}, 'keystone', 'internal', 'hostname', [$service_endpoint])
+    $auth_api_version   = 'v2.0'
+    $identity_uri       = "${internal_auth_protocol}://${internal_auth_address}:5000"
+    $auth_url           = "${identity_uri}/${auth_api_version}"
+
 
 # Set the environment
     Exec {
@@ -32,7 +39,7 @@ class contrail::controller::aggregate {
         "OS_TENANT_NAME=${keystone_tenant}",
         "OS_USERNAME=${keystone_user}",
         "OS_PASSWORD=${nova_hash['user_password']}",
-        "OS_AUTH_URL=http://${service_endpoint}:5000/v2.0/",
+        "OS_AUTH_URL=${auth_url}",
         'OS_ENDPOINT_TYPE=internalURL',
         "OS_REGION_NAME=${region}",
         'NOVA_ENDPOINT_TYPE=internalURL',
