83f69e7db8256181bd7e0bec7b9dd83a3fe2ce3e
Illia Polliul <ipolliul@mirantis.com>
2016-06-18 10:07:56 +0300

    Fix for DPDK controller addition
    When DPDK vrouter is started, interface from network_scheme is not
    present in the system, and this causes L23network scripts failures
    
    Change-Id: I5a40c20ac1a37b69014ee6ded391e15714f6535d
    Signed-off-by: Illia Polliul <ipolliul@mirantis.com>

--- deployment_scripts/puppet/manifests/contrail-compute-repo.pp
+++ deployment_scripts/puppet/manifests/contrail-compute-repo.pp
@@ -31,7 +31,17 @@ if $contrail::compute_dpdk_enabled {
     command => 'sed -i "/nova-compute-.{libvirt_virt_type}/,+5d" libvirt.pp',
   }
 
+  $configure_default_route = '/etc/puppet/modules/osnailyfacter/modular/netconfig/configure_default_route.pp'
+  $netconfig = '/etc/puppet/modules/osnailyfacter/modular/netconfig/netconfig.pp'
+
   exec {'no_defualt_route_reconfigure':
-    command => '/bin/echo "#NOOP here. Modified by contrail plugin" > /etc/puppet/modules/osnailyfacter/modular/netconfig/configure_default_route.pp',
+    command => "/bin/echo '#NOOP here. Modified by contrail plugin' > ${configure_default_route}",
   }
+
+  exec {'no_netconfig':
+    path    => '/usr/local/bin:/bin:/usr/bin/',
+    command => "/bin/echo '#NOOP here. Modified by contrail plugin' > ${netconfig}",
+    onlyif  => 'dpkg -l | grep contrail-vrouter-dpdk-init'
+  }
+
 }
