214c80d2b1369eae2ab39b221236f8004b480029
Pavel Basov <pbasov@mirantis.com>
2016-06-20 11:16:26 -0500

    Make adding compute to hpgs aggregate idempotent
      * TODO: switch to nova provider
    
    Change-Id: I09407153595d461aa02d6af0ac516103a760d025

--- deployment_scripts/puppet/modules/contrail/manifests/compute/aggregate.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/compute/aggregate.pp
@@ -42,8 +42,9 @@ class contrail::compute::aggregate {
     }
 
     exec { 'aggr_add_host':
-      command => "bash -c \"nova aggregate-add-host hpgs-aggr ${::fqdn}\"",
-      unless  => "bash -c \"nova aggregate-details hpgs-aggr | grep ${::fqdn}\"",
+      command => "bash -c \"nova aggregate-add-host hpgs-aggr ${::fqdn}\" && touch /opt/contrail/aggr_add_host-DONE",
+      unless  => "bash -c \"nova aggregate-details hpgs-aggr | grep -q ${::fqdn}\"",
+      creates => '/opt/contrail/aggr_add_host-DONE',
     }
   }
 }
