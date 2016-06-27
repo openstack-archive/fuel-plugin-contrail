91df3bbd419b8113b1961a696e842a4bfef8ffa4
Andrey Kirilochkin <akirilochkin@mirantis.com>
2016-06-27 18:00:52 -0500

    Rewrite ini-file-configs into providers
    Plugin refactoring
    
    Change-Id: Iece2e5341727064ec61b8beaa9a73317399448d7

--- deployment_scripts/puppet/modules/contrail/manifests/analytics.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/analytics.pp
@@ -14,7 +14,7 @@
 
 class contrail::analytics {
 
-# Resources defaults
+  # Resources defaults
   Package { ensure => present }
 
   File {
@@ -31,45 +31,100 @@ class contrail::analytics {
     package_name => 'contrail-analytics',
   }
 
-# Packages
+  # Packages
   package { 'redis-server': } ->
   package { 'contrail-analytics': } ->
   package { 'contrail-openstack-analytics': }
 
-# Analytics config files
+  # Analytics config files
   file { '/etc/redis/redis.conf':
     source  => 'puppet:///modules/contrail/redis.conf',
     owner   => 'root',
     group   => 'root',
     require => Package['redis-server'],
   }
-
-  file { '/etc/contrail/contrail-analytics-api.conf':
-    content => template('contrail/contrail-analytics-api.conf.erb'),
+  contrail_analytics_api_config {
+    'DEFAULTS/host_ip':                    value => $contrail::address;
+    'DEFAULTS/cassandra_server_list':      value => $contrail::cassandra_server_list;
+    'DEFAULTS/http_server_port':           value => '8090';
+    'DEFAULTS/rest_api_port':              value => '9081';
+    'DEFAULTS/rest_api_ip':                value => '0.0.0.0';
+    'DEFAULTS/log_local':                  value => '1';
+    'DEFAULTS/log_level':                  value => 'SYS_NOTICE';
+    'DEFAULTS/log_category':               value => ' ';
+    'DEFAULTS/log_file':                   value => '/var/log/contrail/contrail-analytics-api.log';
+    'DEFAULTS/analytics_data_ttl':         value => '48';
+    'DEFAULTS/analytics_config_audit_ttl': value => '1';
+    'DEFAULTS/analytics_statistics_ttl':   value => '1';
+    'DEFAULTS/analytics_flow_ttl':         value => '-1';
+    'DISCOVERY/disc_server_ip':            value => $contrail::contrail_private_vip;
+    'DISCOVERY/disc_server_port':          value => '5998';
+    'REDIS/redis_server_port':             value => '6379';
+    'REDIS/redis_query_port':              value => '6379';
   }
 
-  file { '/etc/contrail/contrail-collector.conf':
-    content => template('contrail/contrail-collector.conf.erb'),
+  contrail_collector_config {
+    'DEFAULT/analytics_data_ttl':         value => '48';
+    'DEFAULT/analytics_config_audit_ttl': value => '2160';
+    'DEFAULT/analytics_statistics_ttl':   value => '24';
+    'DEFAULT/analytics_flow_ttl':         value => '2';
+    'DEFAULT/cassandra_server_list':      value => $contrail::cassandra_server_list;
+    'DEFAULT/hostip':                     value => $contrail::address;
+    'DEFAULT/log_file':                   value => '/var/log/contrail/contrail-collector.log';
+    'DEFAULT/log_level':                  value => 'SYS_NOTICE';
+    'DEFAULT/log_local':                  value => '1';
+    'DEFAULT/syslog_port':                value => '-1';
+    'COLLECTOR/port':                     value => '8086';
+    'DISCOVERY/server':                   value => $contrail::contrail_private_vip;
+    'REDIS/port':                         value => '6379';
+    'REDIS/server':                       value => '127.0.0.1';
   }
 
-  file { '/etc/contrail/contrail-query-engine.conf':
-    content => template('contrail/contrail-query-engine.conf.erb'),
+  contrail_query_engine_config {
+    'DEFAULT/cassandra_server_list': value => $contrail::cassandra_server_list;
+    'DEFAULT/collectors':            value => '127.0.0.1:8086';
+    'DEFAULT/hostip':                value => '$__contrail_host_ip__';
+    'DEFAULT/http_server_port':      value => '8091';
+    'DEFAULT/log_file':              value => '/var/log/contrail/contrail-query-engine.log';
+    'DEFAULT/log_level':             value => 'SYS_NOTICE';
+    'DEFAULT/log_local':             value => '1';
+    'REDIS/port':                    value => '6379';
+    'REDIS/server':                  value => '127.0.0.1';
   }
 
-  file { '/etc/contrail/contrail-analytics-nodemgr.conf':
-    content => template('contrail/contrail-analytics-nodemgr.conf.erb'),
+  contrail_analytics_nodemgr_config {
+    'DISCOVERY/server': value => $contrail::contrail_private_vip;
+    'DISCOVERY/port':   value => '5998';
   }
 
-  file { '/etc/contrail/contrail-snmp-collector.conf':
-    content => template('contrail/contrail-snmp-collector.conf.erb'),
+  contrail_snmp_collector_config {
+    'DEFAULTS/log_local':         value => '1';
+    'DEFAULTS/log_level':         value => 'SYS_NOTICE';
+    'DEFAULTS/log_file':          value => '/var/log/contrail/contrail-snmp-collector.log';
+    'DEFAULTS/zookeeper':         value => $contrail::zk_comma;
+    'DISCOVERY/disc_server_ip':   value => $contrail::contrail_private_vip;
+    'DISCOVERY/disc_server_port': value => '5998';
   }
 
-  file { '/etc/contrail/contrail-alarm-gen.conf':
-    content => template('contrail/contrail-alarm-gen.conf.erb'),
+  alarm_gen_config {
+    'DEFAULTS/host_ip':           value => $contrail::address;
+    'DEFAULTS/log_local':         value => '1';
+    'DEFAULTS/log_level':         value => 'SYS_NOTICE';
+    'DEFAULTS/log_file':          value => '/var/log/contrail/contrail-alarm-gen.log';
+    'DEFAULTS/kafka_broker_list': value => $contrail::kafka_broker_list;
+    'DEFAULTS/zk_list':           value => $contrail::zk_list;
+    'DISCOVERY/disc_server_ip':   value => $contrail::contrail_private_vip;
+    'DISCOVERY/disc_server_port': value => '5998';
+    'REDIS/redis_server_port':    value => '6379';
   }
 
-  file { '/etc/contrail/contrail-topology.conf':
-    content => template('contrail/contrail-topology.conf.erb'),
+  contrail_topology_config {
+    'DEFAULTS/log_local':         value => '1';
+    'DEFAULTS/log_level':         value => 'SYS_NOTICE';
+    'DEFAULTS/log_file':          value => '/var/log/contrail/contrail-topology.log';
+    'DEFAULTS/zookeeper':         value => $contrail::zk_comma;
+    'DISCOVERY/disc_server_ip':   value => $contrail::contrail_private_vip;
+    'DISCOVERY/disc_server_port': value => '5998';
   }
 
   ini_setting { 'analytics-fdlimit':
@@ -81,7 +136,7 @@ class contrail::analytics {
     require => Package['contrail-analytics'],
   }
 
-# Services
+  # Services
   service { 'redis-server':
     ensure    => running,
     enable    => true,
@@ -92,17 +147,11 @@ class contrail::analytics {
   service { 'supervisor-analytics':
     ensure    => $contrail::service_ensure,
     enable    => true,
-    require   => [Package['contrail-openstack-analytics'],
-                    Service['redis-server'],
-                    Ini_setting['analytics-fdlimit']],
-    subscribe => [File['/etc/contrail/contrail-analytics-api.conf'],
-                    File['/etc/contrail/contrail-collector.conf'],
-                    File['/etc/contrail/contrail-query-engine.conf'],
-                    File['/etc/contrail/contrail-analytics-nodemgr.conf'],
-                    File['/etc/contrail/contrail-snmp-collector.conf'],
-                    File['/etc/contrail/contrail-alarm-gen.conf'],
-                    File['/etc/contrail/contrail-topology.conf'],
-                    ],
+    require   => [
+      Package['contrail-openstack-analytics'],
+      Service['redis-server'],
+      Ini_setting['analytics-fdlimit']
+    ],
   }
 
   # Cron job for transfer contrail-logs to Fuel master
@@ -127,5 +176,11 @@ class contrail::analytics {
       ],
     }
   }
-
+  Contrail_analytics_nodemgr_config <||> ~> Service['supervisor-analytics']
+  Contrail_topology_config <||> ~>          Service['supervisor-analytics']
+  Contrail_snmp_collector_config <||> ~>    Service['supervisor-analytics']
+  Contrail_query_engine_config <||> ~>      Service['supervisor-analytics']
+  Contrail_analytics_api_config <||> ~>     Service['supervisor-analytics']
+  Contrail_collector_config <||> ~>         Service['supervisor-analytics']
+  Alarm_gen_config <||> ~>                  Service['supervisor-analytics']
 }
