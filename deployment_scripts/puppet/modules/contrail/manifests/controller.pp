b2aed0e0d375d2b13a2ea38fa3926a866dbbb196
Andrey Kirilochkin <akirilochkin@mirantis.com>
2016-07-02 09:43:25 -0500

    Heat Engine misconfiguration with Contrail
    DE213689
    
    Change-Id: Ia4d21615b2942cafcf710354c344edfe0a13ae81

--- deployment_scripts/puppet/modules/contrail/manifests/controller.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/controller.pp
@@ -74,7 +74,7 @@ class contrail::controller {
 
 # Contrail-specific heat templates settings
   heat_config {
-    'DEFAULT/plugin_dirs': value => '/usr/lib/heat,/usr/lib/python2.7/dist-packages/contrail_heat,/usr/lib/python2.7/dist-packages/vnc_api/gen/heat/resources';
+    'DEFAULT/plugin_dirs': value => '/usr/lib/python2.7/dist-packages/vnc_api/gen/heat/resources,/usr/lib/python2.7/dist-packages/contrail_heat/resources';
     'clients_contrail/contrail-user': value=> 'neutron';
     'clients_contrail/user': value=> $contrail::neutron_user;
     'clients_contrail/password': value=> $contrail::service_token;
