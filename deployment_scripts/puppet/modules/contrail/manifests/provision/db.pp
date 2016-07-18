f3bfcbb705792bf031990f2faf40125d396660f1
Illia Polliul <ipolliul@mirantis.com>
2016-07-18 16:32:20 +0300

    Contrail API check script updated
    Change-Id: Iace1917d610b8018b159bd21b9242bdb8e5e2a2d
    Closes-bug: #1603959
    Signed-off-by: Illia Polliul <ipolliul@mirantis.com>

--- deployment_scripts/puppet/modules/contrail/manifests/provision/db.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/provision/db.pp
@@ -28,11 +28,10 @@ class contrail::provision::db {
 
   if !defined(Exec['wait_for_api']) {
     exec {'wait_for_api':
-      command   => "if [ `curl --silent --output /dev/null --write-out \"%{http_code}\" http://${contrail::contrail_mgmt_vip}:${contrail::api_server_port}` -lt 401 ];\
-      then exit 1; fi",
+      command   => "bash -c 'if ! [[ $(curl -s -o /dev/null -w \"%{http_code}\" http://${contrail::contrail_mgmt_vip}:${contrail::api_server_port}) =~ ^(200|401)$ ]];\
+then exit 1; fi'",
       tries     => 10,
       try_sleep => 10,
-      notify    => Exec['prov_database_node'],
     }
   }
 
@@ -43,5 +42,6 @@ class contrail::provision::db {
 --admin_user '${contrail::neutron_user}' --admin_tenant_name '${contrail::service_tenant}' --admin_password '${contrail::service_token}' \
 && touch /opt/contrail/prov_database_node-DONE",
     creates => '/opt/contrail/prov_database_node-DONE',
+    require => Exec['wait_for_api'],
   }
 }
