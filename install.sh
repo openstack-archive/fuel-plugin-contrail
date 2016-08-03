9bc30be204f00fb69082c7dc0e20e09131ea6dd5
Vitalii Kovalchuk <vkovalchuk@mirantis.com>
2016-08-03 16:09:55 +0300

    Fix shellcheck warnings in install.sh and upgrade.sh
    Change-Id: Ica0c09c8bc7c154b6a5bbdeb6473900ab8da2553

--- install.sh
+++ install.sh
@@ -28,9 +28,9 @@ set -ex
 
 PLUGIN_PATH="/var/www/nailgun/plugins/contrail-4.0"
 #Now uses the latest package file
-UBUNTU_PKG=`find $PLUGIN_PATH -maxdepth 1 -name 'contrail-install-packages*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4`
-CENTOS_PKG=`find $PLUGIN_PATH -maxdepth 1 -name 'contrail-install-packages*.rpm' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4`
-VMWARE_PKG=`find $PLUGIN_PATH -maxdepth 1 -name 'contrail-install-vcenter-plugin*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4`
+UBUNTU_PKG=$(find $PLUGIN_PATH -maxdepth 1 -name 'contrail-install-packages*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4)
+CENTOS_PKG=$(find $PLUGIN_PATH -maxdepth 1 -name 'contrail-install-packages*.rpm' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4)
+VMWARE_PKG=$(find $PLUGIN_PATH -maxdepth 1 -name 'contrail-install-vcenter-plugin*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4)
 
 yum -y install dpkg dpkg-dev createrepo
 
@@ -45,38 +45,38 @@ fi
 
 if [ -f "$UBUNTU_PKG" ];
 then
-  DEB=`mktemp -d`
-  dpkg -x $UBUNTU_PKG $DEB
+  DEB=$(mktemp -d)
+  dpkg -x "$UBUNTU_PKG" "$DEB"
   cd $PLUGIN_PATH/repositories/ubuntu/
   find . -type f -name "*.deb" -delete
-  tar -xzvf $DEB/opt/contrail/contrail_packages/contrail_debs.tgz -C $PLUGIN_PATH/repositories/ubuntu/
+  tar -xzvf "$DEB"/opt/contrail/contrail_packages/contrail_debs.tgz -C "$PLUGIN_PATH"/repositories/ubuntu/
   if [ -f "$VMWARE_PKG" ];
   then
-    cp $VMWARE_PKG $PLUGIN_PATH/repositories/ubuntu/
-    DEB2=`mktemp -d`
-    dpkg -x $VMWARE_PKG $DEB2
-    cp $DEB2/opt/contrail/contrail_vcenter_plugin_install_repo/*.deb $PLUGIN_PATH/repositories/ubuntu/
+    cp "$VMWARE_PKG" "$PLUGIN_PATH"/repositories/ubuntu/
+    DEB2=$(mktemp -d)
+    dpkg -x "$VMWARE_PKG" "$DEB2"
+    cp "$DEB2"/opt/contrail/contrail_vcenter_plugin_install_repo/*.deb "$PLUGIN_PATH"/repositories/ubuntu/
   fi
-  DPDK_PKG=`find $PLUGIN_PATH -maxdepth 4 -name 'dpdk-depends-packages*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4`
+  DPDK_PKG=$(find $PLUGIN_PATH -maxdepth 4 -name 'dpdk-depends-packages*.deb' -exec stat -c "%y %n" {} + | sort -r | head -n 1 | cut -d' ' -f 4)
   if [ -f "$DPDK_PKG" ];
   then
-    DEB3=`mktemp -d`
-    cp $DPDK_PKG $DEB3
-    cd $DEB3 && ar vx dpdk-depends-packages*.deb && tar xf data.tar.xz && cp $DEB3/opt/contrail/contrail_install_repo_dpdk/contrail-dpdk-kernel-modules-dkms*.deb $PLUGIN_PATH/repositories/ubuntu/
+    DEB3=$(mktemp -d)
+    cp "$DPDK_PKG" "$DEB3"
+    cd "$DEB3" && ar vx dpdk-depends-packages*.deb && tar xf data.tar.xz && cp "$DEB3"/opt/contrail/contrail_install_repo_dpdk/contrail-dpdk-kernel-modules-dkms*.deb "$PLUGIN_PATH"/repositories/ubuntu/
   fi
-  cd $PLUGIN_PATH/repositories/ubuntu/
+  cd "$PLUGIN_PATH"/repositories/ubuntu/
   dpkg-scanpackages ./ | gzip -c - > Packages.gz
 fi
 
 if [ -f "$CENTOS_PKG" ];
 then
-  RPM=`mktemp -d`
-  cd $RPM
-  rpm2cpio $CENTOS_PKG | cpio -id
-  mkdir -p $PLUGIN_PATH/repositories/centos/Packages
-  cd $PLUGIN_PATH/repositories/centos/
+  RPM=$(mktemp -d)
+  cd "$RPM"
+  rpm2cpio "$CENTOS_PKG" | cpio -id
+  mkdir -p "$PLUGIN_PATH"/repositories/centos/Packages
+  cd "$PLUGIN_PATH"/repositories/centos/
   find Packages -type f -name "*.rpm" -delete
-  tar -xzvf $RPM/opt/contrail/contrail_packages/contrail_rpms.tgz -C $PLUGIN_PATH/repositories/centos/Packages/
+  tar -xzvf "$RPM"/opt/contrail/contrail_packages/contrail_rpms.tgz -C "$PLUGIN_PATH"/repositories/centos/Packages/
   createrepo .
 fi
 
