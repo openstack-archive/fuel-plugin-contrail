44d89f319e02d6d63b4c4409c0f175da356eff9a
Illia Polliul <ipolliul@mirantis.com>
2016-09-19 12:39:58 +0300

    Ensure user_token is not present in api-paste.ini for cloud-admin
    Change-Id: I2cc64bd4551d16ef36e596f3cad7417679218c1d
    Closes-bug: #1625080
    Signed-off-by: Illia Polliul <ipolliul@mirantis.com>

--- deployment_scripts/puppet/modules/contrail/manifests/controller.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/controller.pp
@@ -172,6 +172,16 @@ class contrail::controller {
       value   => 'neutron_plugin_contrail.plugins.opencontrail.neutron_middleware:token_factory',
       notify  => Service['neutron-server'],
     }
+    else {
+      ini_setting {'no_user_token':
+        ensure  => present,
+        path    => '/etc/neutron/api-paste.ini',
+        section => 'composite:neutronapi_v2_0',
+        setting => 'keystone',
+        value   => 'cors request_id catch_errors authtoken keystonecontext extensions neutronapiapp_v2_0',
+        notify  => Service['neutron-server'],
+      }
+    }
   }
 
   Neutron_config <||> ~> Service <|title == 'neutron-server'|>
