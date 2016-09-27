7584829243ab3de8aee4c903e0f3934da2b5b51c
otsvigun <otsvigun@mirantis.com>
2016-09-27 09:04:53 +0300

    Remove logs directory from path of vSrx template.
    -remove logs directory from path of vSrx template
    -add vsrx condition to system and functional tests
    
    Change-Id: Iea7e2a945748e7a3643de0a6ba5ba209c560a4af

--- plugin_test/helpers/vsrx.py
+++ plugin_test/helpers/vsrx.py
@@ -51,13 +51,13 @@ def activate(
     map(call_cmd, [
         'virsh destroy vSRX1',
         'virsh undefine vSRX1',
-        'sed -r "s/ENV_NAME/$ENV_NAME/g" {0} > logs/vSRX1.xml'.format(
+        'sed -r "s/ENV_NAME/$ENV_NAME/g" {0} > vSRX1.xml'.format(
             settings.VSRX_TEMPLATE_PATH),
-        'sed -i -r "s/vSRX1.img/vSRX.400.img/g" logs/vSRX1.xml',
+        'sed -i -r "s/vSRX1.img/vSRX.400.img/g" vSRX1.xml',
     ])
 
     logger.info("#" * 10 + 'Create vSRX...' + "#" * 10)
-    if subprocess.call('virsh create logs/vSRX1.xml', shell=True):
+    if subprocess.call('virsh create vSRX1.xml', shell=True):
         logger.info("#" * 10 + 'VSRX could not be established, '
                     'OSTF will not be running' + "#" * 10)
         return False
