73f8984ab199a489192cf450db9831be3da971d1
Vlad Naboichenko <vnaboychenko@mirantis.com>
2016-11-08 11:20:17 -0800

    Use juniper qemu and libvirt for all compute nodes
    Change-Id: I9317c1ec61ccae8690bb4ba3a07c85d204cb83a1

--- deployment_scripts/puppet/modules/contrail/manifests/compute/compute_override.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/compute/compute_override.pp
@@ -33,121 +33,117 @@ class contrail::compute::compute_override {
     packages    => $common_pkg,
   }
 
-  if $contrail::compute_dpdk_enabled {
-    # Create local dpdk repository
-    package { 'dpdk-depends-packages':
+  package { 'dpdk-depends-packages':
+    ensure => present,
+  } ->
+  exec { 'update-dpdk-repo':
+    command => 'dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz',
+    cwd     => '/opt/contrail/contrail_install_repo_dpdk',
+  } ->
+  file {'/opt/contrail/contrail_install_repo_dpdk/Release':
+    ensure  => file,
+    content => 'Label: dpdk-depends-packages',
+  } ->
+  apt::source { 'dpdk-depends-repo':
+    location    => 'file:/opt/contrail/contrail_install_repo_dpdk',
+    repos       => './',
+    release     => ' ',
+    include_src => false,
+  }
+
+  # Patch nova packages if it set on Fuel UI
+  if $contrail::patch_nova {
+    apt::pin { 'contrail-pin-nova':
+      explanation => 'Prevent patched python-nova from upgrades',
+      priority    => 1200,
+      label       => '1:2015.1.1-1~u14.04+mos19665',
+      packages    => 'python-nova',
+    } ->
+    file { "${patch_path}/nova-dpdk-vrouter.patch":
       ensure => present,
-    } ->
-    exec { 'update-dpdk-repo':
-      command => 'dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz',
-      cwd     => '/opt/contrail/contrail_install_repo_dpdk',
-    } ->
-    file {'/opt/contrail/contrail_install_repo_dpdk/Release':
-      ensure  => file,
-      content => 'Label: dpdk-depends-packages',
-    } ->
-    apt::source { 'dpdk-depends-repo':
-      location    => 'file:/opt/contrail/contrail_install_repo_dpdk',
-      repos       => './',
-      release     => ' ',
-      include_src => false,
+      mode   => '0644',
+      source => 'puppet:///modules/contrail/nova-dpdk-vrouter.patch',
+    }->
+    exec { 'patch-python-nova':
+      command => 'patch -p1 < nova-dpdk-vrouter.patch && touch python-nova-patch-DONE',
+      cwd     => $patch_path,
+      creates => "${patch_path}/python-nova-patch-DONE",
     }
+  }
 
-    # Patch nova packages if it set on Fuel UI
-    if $contrail::patch_nova {
-      apt::pin { 'contrail-pin-nova':
-        explanation => 'Prevent patched python-nova from upgrades',
-        priority    => 1200,
-        label       => '1:2015.1.1-1~u14.04+mos19665',
-        packages    => 'python-nova',
-      } ->
-      file { "${patch_path}/nova-dpdk-vrouter.patch":
-        ensure => present,
-        mode   => '0644',
-        source => 'puppet:///modules/contrail/nova-dpdk-vrouter.patch',
-      }->
-      exec { 'patch-python-nova':
-        command => 'patch -p1 < nova-dpdk-vrouter.patch && touch python-nova-patch-DONE',
-        cwd     => $patch_path,
-        creates => "${patch_path}/python-nova-patch-DONE",
+  # Override libvirt and qemu packages if it set on Fuel UI
+  if $contrail::install_contrail_qemu_lv {
+
+
+    if !defined(Service['libvirtd']) {
+      service { 'libvirtd':
+        ensure  => stopped,
+        enable  => false,
+        before => Exec['override-libvirt'],
+      }
+    }
+    else {
+      Service <| (title == 'libvirtd') |> {
+        ensure => stopped,
+        enable => false,
       }
     }
 
-    # Override libvirt and qemu packages if it set on Fuel UI
-    if $contrail::install_contrail_qemu_lv {
-      # For qemu you don't need additional pinning, only dpdk repository
-      apt::pin { 'contrail-pin-qemu':
-        explanation => 'Install qemu from dpdk-depends',
-        priority    => 1200,
-        label       => 'dpdk-depends-packages',
-        packages    => $qemu_pkg,
-      } ->
-      exec {'override-qemu':
-        command => "apt-get install --yes ${keep_config_files} ${force_overwrite} \
+    # For qemu you don't need additional pinning, only dpdk repository
+    apt::pin { 'contrail-pin-qemu':
+      explanation => 'Install qemu from dpdk-depends',
+      priority    => 1200,
+      label       => 'dpdk-depends-packages',
+      packages    => $qemu_pkg,
+    } ->
+    exec {'override-qemu':
+      command => "apt-get install --yes ${keep_config_files} ${force_overwrite} \
 qemu-kvm qemu-system-x86 qemu-system-common",
-        unless  => 'dpkg -l | grep qemu-system-common | grep contrail',
-        require => Apt::Source['dpdk-depends-repo'],
+      unless  => 'dpkg -l | grep qemu-system-common | grep contrail',
+      require => Apt::Source['dpdk-depends-repo'],
+    }
+    if !defined(Service['qemu-kvm']) {
+      service { 'qemu-kvm':
+        ensure  => running,
+        enable  => true,
       }
-      if !defined(Service['qemu-kvm']) {
-        service { 'qemu-kvm':
-          ensure  => running,
-          enable  => true,
-          require => Exec['override-qemu'],
-        }
-      }
-      apt::pin { 'contrail-override-libvirt':
-        explanation => 'Set priority for packages that need to override from contrail repository',
-        packages    => $libvirt_pkg,
-        priority    => 1200,
-        label       => 'dpdk-depends-packages',
-        require     => Service['qemu-kvm'],
-      } ->
-      # Install options are supported starting from puppet 3.5.1
-      #package { $libvirt_pkg:
-      #  ensure          => '2:1.2.12-0ubuntu7+contrail2',
-      #  install_options => [$keep_config_files, $force_overwrite],
-      #} ->
-      exec { 'override-libvirt':
-        command => "apt-get install --yes ${keep_config_files} ${force_overwrite} libvirt-bin libvirt0",
-        unless  => 'dpkg -l | grep libvirt0 | grep contrail',
-        require => Apt::Source['dpdk-depends-repo'],
-      }
-      # With a new libvirt packages this init script must be stopped
-      if !defined(Service['libvirtd']) {
-        service { 'libvirtd':
-          ensure  => stopped,
-          enable  => false,
-          require => Exec['override-libvirt'],
-        }
-      }
-      else {
-        Service <| (title == 'libvirtd') |> {
-          ensure => stopped,
-          enable => false,
-        }
-      }
-      # This options must be uncommented for correct work with nova-compute
-      file_line { 'add_libvirt_opt1':
-        path    => '/etc/libvirt/libvirtd.conf',
-        match   => 'auth_unix_ro',
-        line    => 'auth_unix_ro = "none"',
-        require => Service['libvirtd'],
-      } ->
-      file_line { 'add_libvirt_opt2':
-        path  => '/etc/libvirt/libvirtd.conf',
-        match => 'auth_unix_rw',
-        line  => 'auth_unix_rw = "none"',
-      } ~>
-      service { $contrail::libvirt_name:
-        ensure => running,
-        enable => true,
-      }
-      if !defined(Service['nova-compute']) {
-        service { 'nova-compute':
-          ensure  => running,
-          enable  => true,
-          require => Service[$contrail::libvirt_name],
-        }
+    }
+    apt::pin { 'contrail-override-libvirt':
+      explanation => 'Set priority for packages that need to override from contrail repository',
+      packages    => $libvirt_pkg,
+      priority    => 1200,
+      label       => 'dpdk-depends-packages',
+      require     => Service['qemu-kvm'],
+    } ->
+    # Install options are supported starting from puppet 3.5.1
+    exec { 'override-libvirt':
+      command => "apt-get install --yes ${keep_config_files} ${force_overwrite} libvirt-bin libvirt0",
+      unless  => 'dpkg -l | grep libvirt0 | grep contrail',
+      require => Apt::Source['dpdk-depends-repo'],
+    }
+    # With a new libvirt packages this init script must be stopped
+    # This options must be uncommented for correct work with nova-compute
+    file_line { 'add_libvirt_opt1':
+      path    => '/etc/libvirt/libvirtd.conf',
+      match   => 'auth_unix_ro',
+      line    => 'auth_unix_ro = "none"',
+      require => Service['libvirtd'],
+    } ->
+    file_line { 'add_libvirt_opt2':
+      path  => '/etc/libvirt/libvirtd.conf',
+      match => 'auth_unix_rw',
+      line  => 'auth_unix_rw = "none"',
+    } ~>
+    service { $contrail::libvirt_name:
+      ensure => running,
+      enable => true,
+      require => Exec['override-libvirt']
+    }
+    if !defined(Service['nova-compute']) {
+      service { 'nova-compute':
+        ensure  => running,
+        enable  => true,
+        require => Service[$contrail::libvirt_name],
       }
     }
   }
