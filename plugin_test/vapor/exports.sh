f546ff10be06893f1dd51361821d18c0a3bc87ba
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-12-16 16:45:03 +0300

    Add test_contrail_node_services_status
    Change-Id: Ia2cc0ab660ab45e9a5b1afd02ef5834edbfba3dc

--- plugin_test/vapor/exports.sh
+++ plugin_test/vapor/exports.sh
@@ -5,6 +5,11 @@ export OS_PASSWORD=secret
 export OS_TENANT_NAME=admin
 export OS_AUTH_URL=http://127.0.0.1:5000/v2.0
 
+# os-faults config
+export OS_FAULTS_CLOUD_DRIVER=tcpcloud
+export OS_FAULTS_CLOUD_DRIVER_ADDRESS=192.168.10.100
+export OS_FAULTS_CLOUD_DRIVER_KEYFILE=/home/jenkins/cloud.key
+
 #export OS_PROJECT_DOMAIN_NAME=default
 #export OS_USER_DOMAIN_NAME=default
 #export OS_PROJECT_NAME=admin
