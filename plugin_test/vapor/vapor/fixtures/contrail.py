6b4b3192acd8463fb3bc936b8b1808c7ca1161ae
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-12-19 10:43:49 +0300

    Add contrail network create/update/delete tests
    Change-Id: I27877a77d781c2ca49e8d1b9d526b697b2e65a8c

--- plugin_test/vapor/vapor/fixtures/contrail.py
+++ plugin_test/vapor/vapor/fixtures/contrail.py
@@ -1,12 +1,15 @@
+import pycontrail.client as client
 import pytest
+from six.moves.urllib import parse
 
-from vapor.settings import CONTRAIL_CREDS
+from vapor import settings
 from vapor.helpers.clients import ContrailClient
 
 
 @pytest.yield_fixture
 def client_contrail():
-    with ContrailClient(CONTRAIL_CREDS['controller_addr']) as contrail:
+    with ContrailClient(settings.CONTRAIL_CREDS[
+            'controller_addr']) as contrail:
         yield contrail
     print('helpers.clients.client_contrail')
 
@@ -15,3 +18,16 @@ def client_contrail():
 def contrail_nodes(os_faults_steps):
     """Returns all nodes which have contrail-status command."""
     return os_faults_steps.get_nodes_by_cmd('which contrail-status')
+
+
+@pytest.fixture
+def contrail_api_client(session):
+    """Return instance of contail client."""
+    neutron_endpoint = session.get_endpoint(service_type='network')
+    parse_result = parse.urlparse(neutron_endpoint)
+    contail_endpoint = parse_result._replace(netloc='{}:{}'.format(
+        parse_result.hostname, settings.CONTAIL_API_PORT)).geturl()
+    headers = {'Content-type': 'application/json; charset="UTF-8"'}
+    headers.update(session.get_auth_headers())
+    return client.Client(
+        url=contail_endpoint, headers=headers)
