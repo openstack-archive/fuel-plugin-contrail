30c463023e4e8db3d1cd74507a9aad41de9ad647
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-12-19 13:08:19 +0300

    Add contrail network policy create/update/delete tests
    Change-Id: I9546767f7e524b9942eb85cd3c71fd4c8ef230e9

--- plugin_test/vapor/vapor/conftest.py
+++ plugin_test/vapor/vapor/conftest.py
@@ -4,4 +4,5 @@ from stepler.conftest import *  # noqa
 
 from vapor.fixtures.contrail import *  # noqa
 from vapor.fixtures.networks import *  # noqa
+from vapor.fixtures.policies import *  # noqa
 from vapor.fixtures.security_groups import *  # noqa
---
+++ plugin_test/vapor/vapor/fixtures/policies.py
@@ -0,0 +1,30 @@
+import pycontrail.types as types
+import pytest
+from stepler.third_party import utils
+
+
+@pytest.fixture(autouse=True)
+def policies_cleanup(contrail_api_client):
+    """Cleanup created policies after test."""
+
+    def _get_policies_uuids():
+        return {
+            net['uuid']
+            for net in contrail_api_client.network_policys_list()[
+                'network-policys']
+        }
+
+    before = _get_policies_uuids()
+
+    yield
+
+    for net_uuid in _get_policies_uuids() - before:
+        contrail_api_client.network_policy_delete(id=net_uuid)
+
+
+@pytest.fixture
+def network_policy(contrail_api_client):
+    policy_name, = utils.generate_ids()
+    policy = types.NetworkPolicy(policy_name)
+    contrail_api_client.network_policy_create(policy)
+    return policy
