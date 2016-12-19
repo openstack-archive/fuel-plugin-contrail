a283f2249fa18be8efd5128de48dddb29c892580
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-12-19 11:23:12 +0300

    Add contrail security groups create/update/delete tests
    Change-Id: Id48c368277047cff770d46d020865b88d521deff

--- plugin_test/vapor/vapor/conftest.py
+++ plugin_test/vapor/vapor/conftest.py
@@ -4,3 +4,4 @@ from stepler.conftest import *  # noqa
 
 from vapor.fixtures.contrail import *  # noqa
 from vapor.fixtures.networks import *  # noqa
+from vapor.fixtures.security_groups import *  # noqa
