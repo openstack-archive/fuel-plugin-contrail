1a694417f17e6e255fd7e4cefda04c6a50c1c75d
Pavel Chechetin <pchechetin@mirantis.com>
2016-12-20 11:53:43 -0800

    Wait until all tables for anaylytics are ready
    During starting contrail-collector creates all tables in Cassandra.
    This process shouldn't take place on all contrail-analytics nodes, because it
    may break Cassandra DB.
    
    To prevent conurrent tables creating, wait_for_cassandra_keyspace is added.
    
    Change-Id: I1a7302414abd542f24f944684c5a4810dbc2b8ad

--- deployment_scripts/puppet/modules/contrail/manifests/init.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/init.pp
@@ -226,4 +226,31 @@ class contrail {
 
   $aaa_mode = pick($settings['aaa_mode'], 'cloud-admin')
 
+  $analytics_keyspace = "ContrailAnalyticsCql"
+  $analytics_tables = [
+    "flowrecordtable",
+    "flowtablevrouterver2",
+    "statstablebydbltagv3",
+    "objectvaluetable",
+    "messagetablemessagetype",
+    "messagetablekeyword",
+    "messagetabletimestamp",
+    "messagetablecategory",
+    "statstablebyu64strtagv3",
+    "statstablebyu64tagv3",
+    "messagetablesource",
+    "statstablebystrstrtagv3",
+    "systemobjecttable",
+    "flowtableprotdpver2",
+    "statstablebystru64tagv3",
+    "objecttable",
+    "statstablebyu64u64tagv3",
+    "flowtabledvndipver2",
+    "messagetable",
+    "statstablebystrtagv3",
+    "flowtableprotspver2",
+    "flowtablesvnsipver2",
+    "messagetablemoduleid"
+  ]
+
 }
