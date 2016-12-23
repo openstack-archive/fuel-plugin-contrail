17be83d8b4f21791fc6c28dbd14b9720c312691e
Georgy Dyuldin <g.dyuldin@gmail.com>
2016-12-23 15:31:01 +0300

    Add test_network_deleting_with_server
    Change-Id: Ie07565e46f71b3e9eb5f200f7ef147fa07153221

---
+++ plugin_test/vapor/vapor/tests/test_common.py
@@ -0,0 +1,9 @@
+from hamcrest import assert_that, calling, raises  # noqa H301
+from pycontrail import exceptions
+
+
+def test_network_deleting_with_server(network, server, contrail_api_client):
+    assert_that(
+        calling(contrail_api_client.virtual_network_delete).with_args(
+            id=network['id']),
+        raises(exceptions.RefsExistError))
