db04b0abb692549f682cb24a1e5634dd269edef0
Uday T Kumar <uk310y@att.com>
2017-01-05 16:59:27 +0530

    Update contrail plugin to add default api and wrapper to add RBAC rules to contrail using rbacutil.py
    Change-Id: I3419accdbdbc353c4c36344496d7a15cb43e4b2c

---
+++ deployment_scripts/puppet/modules/contrail/manifests/rbac_settings.pp
@@ -0,0 +1,43 @@
+#    Copyright 2016 AT&T Corp
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+class contrail::rbac_settings {
+
+  $rbac_wrapper_path = '/var/lib/fuel/rbac_wrapper'
+  $hiera_data_dir    = '/etc/hiera'
+  $rbac_rules        = $contrail::settings['rbac_rules']
+
+  file { $rbac_wrapper_path:
+    ensure => directory,
+  }
+
+  file { "${rbac_wrapper_path}/rbac_wrapper.py":
+    ensure  => present,
+    content => template('contrail/rbac_wrapper.py.erb'),
+    require => File[$rbac_wrapper_path]
+  }
+
+  file { "${rbac_wrapper_path}/rbac_cmd.sh":
+    ensure  => present,
+    content => template('contrail/rbac_cmd.sh.erb'),
+    require => File[$rbac_wrapper_path],
+  }
+
+  file { "${rbac_wrapper_path}/rbac_settings.yaml":
+    ensure  => present,
+    content => template('contrail/rbac_settings_dump.yaml.erb'),
+    require => File[$rbac_wrapper_path],
+  }
+
+}
