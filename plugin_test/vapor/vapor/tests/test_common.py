06216a2ba45d812f5fb04af85722d7adc154ca0b
Denis Deryabin <dderyabin@mirantis.com>
2017-01-17 00:48:17 +0300

    add test_delete_vm_with_associated_vn
    Change-Id: I7a281ebae518714db698ded339fdc19dc9db0cd8

--- plugin_test/vapor/vapor/tests/test_common.py
+++ plugin_test/vapor/vapor/tests/test_common.py
@@ -10,8 +10,10 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from hamcrest import assert_that, calling, raises  # noqa H301
+from hamcrest import (assert_that, calling, raises,
+                      has_item, has_entry, is_not, empty)  # noqa H301
 from pycontrail import exceptions
+from neutronclient.common import exceptions as neutron_exceptions
 
 
 def test_network_deleting_with_server(network, server, contrail_api_client):
@@ -30,3 +32,27 @@ def test_create_vm_bulk(net_subnet_router, tiny_flavor,
                                           flavor=tiny_flavor,
                                           networks=[network])
     server_steps.delete_servers(servers)
+
+
+def test_delete_vm_with_associated_vn(contrail_network, contrail_subnet,
+                                      tiny_flavor, cirros_image,
+                                      server_steps, network_steps,
+                                      contrail_api_client):
+    servers = server_steps.create_servers(
+        image=cirros_image, flavor=tiny_flavor,
+        nics=[{'net-id': contrail_network.uuid}])
+    net = network_steps.get_network(id=contrail_network.uuid)
+
+    # Check that we can't delete this net.
+    assert_that(calling(network_steps.delete).with_args(net),
+                raises(neutron_exceptions.NetworkInUseClient))
+
+    # Check that network is still there
+    # via contrail API
+    c_networks = contrail_api_client.virtual_networks_list()
+    assert_that(c_networks['virtual-networks'],
+                has_item(has_entry('uuid', contrail_network.uuid)))
+
+    # via neutron API
+    assert_that(network_steps.get_network_by_name(net['name']),
+                is_not(empty()))
