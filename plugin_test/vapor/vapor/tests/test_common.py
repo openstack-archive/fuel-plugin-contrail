2744ff29125f3259a9790d632c3fa7ad521b5499
Denis Deryabin <dderyabin@mirantis.com>
2017-01-17 00:48:17 +0300

    add test_delete_vm_with_associated_vn
    Change-Id: I7a281ebae518714db698ded339fdc19dc9db0cd8

--- plugin_test/vapor/vapor/tests/test_common.py
+++ plugin_test/vapor/vapor/tests/test_common.py
@@ -12,6 +12,7 @@
 
 from hamcrest import assert_that, calling, raises  # noqa H301
 from pycontrail import exceptions
+from neutronclient.common import exceptions as neutron_exceptions
 
 
 def test_network_deleting_with_server(network, server, contrail_api_client):
@@ -29,4 +30,16 @@ def test_create_vm_bulk(net_subnet_router, tiny_flavor,
                                           image=cirros_image,
                                           flavor=tiny_flavor,
                                           networks=[network])
+    server_steps.delete_servers(servers)
+
+
+def test_delete_vm_with_associated_vn(contrail_network, contrail_subnet,
+                                      tiny_flavor, cirros_image,
+                                      server_steps, network_steps):
+    servers = server_steps.create_servers(
+        count=1, image=cirros_image, flavor=tiny_flavor,
+        nics=[{'net-id': contrail_network.uuid}])
+    net = network_steps.get_network(id=contrail_network.uuid)
+    assert_that(calling(network_steps.delete).with_args(net),
+                raises(neutron_exceptions.NetworkInUseClient))
     server_steps.delete_servers(servers)
