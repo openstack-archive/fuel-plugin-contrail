ceab66dd9e862418ee300f97051bba8208f45500
Denis Deryabin <dderyabin@mirantis.com>
2017-01-19 12:28:13 +0300

    add test_metadata_service
    Change-Id: I9db8883f9cac821e07edccbe5e64053603926c9c

--- plugin_test/vapor/vapor/tests/test_common.py
+++ plugin_test/vapor/vapor/tests/test_common.py
@@ -10,7 +10,8 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from hamcrest import assert_that, calling, raises  # noqa H301
+from hamcrest import assert_that, calling, raises, contains_string  # noqa H301
+from stepler import config as stepler_config
 from pycontrail import exceptions
 
 
@@ -30,3 +31,46 @@ def test_create_vm_bulk(net_subnet_router, tiny_flavor,
                                           flavor=tiny_flavor,
                                           networks=[network])
     server_steps.delete_servers(servers)
+
+
+def test_metadata_service(security_group, port_steps,
+                          network, subnet, public_network, flavor,
+                          create_floating_ip, tiny_flavor, cirros_image,
+                          server_steps):
+    """Description: Test to validate metadata service
+       Test steps:
+           1. Create a VN.
+           2. Launch a VM in this VN.
+           3. Write a metadata script to print the current time.
+           4. Pass this during the VM launch.
+       Pass criteria:
+           The output of the metadata script should be seen in the VM.
+    """
+    output_filename = 'output.txt'
+    userdata = (
+        u'#!/bin/sh\n'
+        u'echo "TestMetadataService.'
+        u'The time is now $(date -R)!" | tee /tmp/{filename}\n'.format(
+            filename=output_filename
+        )
+    )
+
+    server, = server_steps.create_servers(
+        image=cirros_image,
+        flavor=flavor,
+        networks=[network],
+        security_groups=[security_group],
+        userdata=userdata,
+        username=stepler_config.CIRROS_USERNAME,
+        password=stepler_config.CIRROS_PASSWORD)
+
+    server1_port = port_steps.get_port(
+        device_owner=stepler_config.PORT_DEVICE_OWNER_SERVER,
+        device_id=server.id)
+    floating_ip = create_floating_ip(public_network, port=server1_port)
+
+    with server_steps.get_server_ssh(server,
+                                     ip=floating_ip['floating_ip_address'],
+                                     ssh_timeout=60) as server_ssh:
+        res = server_ssh.execute(u'ls /tmp/', timeout=60)
+        assert_that(res.stdout, contains_string(output_filename))
