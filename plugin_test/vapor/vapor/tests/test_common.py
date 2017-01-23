6920a2af29ddfab20a89498b2c0434c85d6ece34
Denis Deryabin <dderyabin@mirantis.com>
2017-01-23 13:49:31 +0300

    add test_delete_vm_with_associated_vn add test_two_nets_same_name add test_metadata_service
    Change-Id: I97294ed0a5e7cc9ce4d8cbda65e8ce3c932327d8

--- plugin_test/vapor/vapor/tests/test_common.py
+++ plugin_test/vapor/vapor/tests/test_common.py
@@ -10,8 +10,12 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-from hamcrest import assert_that, calling, raises  # noqa H301
+from hamcrest import (assert_that, calling, raises, contains_string,
+                      has_item, has_entry, is_not, empty)  # noqa H301
+from stepler import config as stepler_config
 from pycontrail import exceptions
+import pycontrail.types as contrail_types
+from neutronclient.common import exceptions as neutron_exceptions
 
 
 def test_network_deleting_with_server(network, server, contrail_api_client):
@@ -23,6 +27,14 @@ def test_network_deleting_with_server(network, server, contrail_api_client):
 
 def test_create_vm_bulk(net_subnet_router, tiny_flavor,
                         cirros_image, server_steps):
+    """Description: Test to validate creation and deletion of VMs in bulk.
+    Test steps:
+        1. Create VMs in bulk, based on the count specified.
+        2. Verify the VMs so created and cleanup of the VMs
+           should also go through fine.
+    Pass criteria:
+        The creation and deletion of the VMs in bulk should go through fine.
+    """
     network, _, _ = net_subnet_router
     BULK_SERVER_COUNT = 10
     servers = server_steps.create_servers(count=BULK_SERVER_COUNT,
@@ -30,3 +42,96 @@ def test_create_vm_bulk(net_subnet_router, tiny_flavor,
                                           flavor=tiny_flavor,
                                           networks=[network])
     server_steps.delete_servers(servers)
+
+
+def test_delete_vm_with_associated_vn(contrail_network, contrail_subnet,
+                                      tiny_flavor, cirros_image,
+                                      server_steps, network_steps,
+                                      contrail_api_client):
+    """Description: Test to validate that VN cannot be deleted if
+        there is a VM associated with it.
+    Test steps:
+        1. Create a VN and launch a VM in that VN.
+        2. Verify that with the VM still existing,
+           it is not possible to delete the VN.
+    Pass criteria:
+        The attempt to delete VN should fail.
+    """
+    servers = server_steps.create_servers(
+        image=cirros_image, flavor=tiny_flavor,
+        nics=[{'net-id': contrail_network.uuid}])
+    net = network_steps.get_network(id=contrail_network.uuid)
+
+    # Check that we can't delete this net.
+    assert_that(calling(network_steps.delete).with_args(net),
+                raises(neutron_exceptions.NetworkInUseClient))
+
+    # Check that network is still there
+    # via contrail API
+    c_networks = contrail_api_client.virtual_networks_list()
+    assert_that(c_networks['virtual-networks'],
+                has_item(has_entry('uuid', contrail_network.uuid)))
+
+    # via neutron API
+    assert_that(network_steps.get_network_by_name(net['name']),
+                is_not(empty()))
+
+
+def test_two_nets_same_name(contrail_api_client, contrail_network,
+                            contrail_subnet):
+    """Description: Test to validate that with the same subnet and
+        name provided, two different VNs cannot be created.
+    Test steps:
+        1. Create a VN.
+        2. Create a second VN with the same name and subnet as the first VN.
+        3. Verify that no second VN object is created.
+    Pass criteria:
+        There is a single VN created.
+    """
+    net = contrail_types.VirtualNetwork(contrail_network.name)
+    assert_that(
+        calling(contrail_api_client.virtual_network_create).with_args(net),
+        raises(exceptions.RefsExistError))
+
+
+def test_metadata_service(security_group, port_steps,
+                          network, subnet, public_network, flavor,
+                          create_floating_ip, tiny_flavor, cirros_image,
+                          server_steps):
+    """Description: Test to validate metadata service
+    Test steps:
+        1. Create a VN.
+        2. Launch a VM in this VN.
+        3. Write a metadata script to print the current time.
+        4. Pass this during the VM launch.
+    Pass criteria:
+        The output of the metadata script should be seen in the VM.
+    """
+    output_filename = 'output.txt'
+    userdata = (
+        u'#!/bin/sh\n'
+        u'echo "TestMetadataService.'
+        u'The time is now $(date -R)!" | tee /tmp/{filename}\n'.format(
+            filename=output_filename
+        )
+    )
+
+    server, = server_steps.create_servers(
+        image=cirros_image,
+        flavor=flavor,
+        networks=[network],
+        security_groups=[security_group],
+        userdata=userdata,
+        username=stepler_config.CIRROS_USERNAME,
+        password=stepler_config.CIRROS_PASSWORD)
+
+    server1_port = port_steps.get_port(
+        device_owner=stepler_config.PORT_DEVICE_OWNER_SERVER,
+        device_id=server.id)
+    floating_ip = create_floating_ip(public_network, port=server1_port)
+
+    with server_steps.get_server_ssh(server,
+                                     ip=floating_ip['floating_ip_address'],
+                                     ssh_timeout=60) as server_ssh:
+        res = server_ssh.execute(u'ls /tmp/', timeout=60)
+        assert_that(res.stdout, contains_string(output_filename))
