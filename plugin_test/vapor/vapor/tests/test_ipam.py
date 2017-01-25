c8285d0cd25c6328c83933aa885bec2eac2d2f90
Georgy Dyuldin <g.dyuldin@gmail.com>
2017-01-25 17:35:20 +0300

    Add test_ipam_ntp_server_setting
    Change-Id: I1bae82fa961590ed4ae841719125560fd87d8017

--- plugin_test/vapor/vapor/tests/test_ipam.py
+++ plugin_test/vapor/vapor/tests/test_ipam.py
@@ -13,6 +13,7 @@
 from hamcrest import assert_that, contains_string
 from pycontrail import types
 from stepler import config as stepler_config
+from stepler.third_party import utils
 
 
 def test_subnet_creating_with_custom_ipam(
@@ -95,3 +96,55 @@ def test_ipam_virtual_dns(
             server, ip=floating_ip['floating_ip_address']) as server_ssh:
         result = server_ssh.execute('nslookup ' + name)
     assert_that(result.stdout, contains_string(ip))
+
+
+def test_ipam_ntp_server_setting(
+        keypair, ubuntu_image, flavor, security_group, public_network,
+        contrail_network, contrail_ipam, contrail_create_subnet,
+        create_floating_ip, server_steps, port_steps, contrail_api_client):
+    """Verify IPAM NTP server IP setting.
+
+    Steps:
+        #. Create IPAM with any NTP server IP address
+        #. Create network with IPAM
+        #. Boot nova server on network
+        #. Check that DHCP reply contains configured NTP server address
+    """
+    ntp_ip, = utils.generate_ips()
+
+    # Create subnet
+    contrail_create_subnet(contrail_network, contrail_ipam)
+
+    # Add NTP server IP to IPAM
+    contrail_ipam.network_ipam_mgmt = types.IpamType(dhcp_option_list={
+        'dhcp_option': [{
+            'dhcp_option_name': '4',
+            'dhcp_option_value': ntp_ip
+        }]
+    })
+    contrail_api_client.network_ipam_update(contrail_ipam)
+
+    # Boot server
+    server = server_steps.create_servers(
+        image=ubuntu_image,
+        flavor=flavor,
+        security_groups=[security_group],
+        nics=[{
+            'net-id': contrail_network.uuid
+        }],
+        keypair=keypair,
+        username=stepler_config.UBUNTU_USERNAME)[0]
+
+    server_port = port_steps.get_port(
+        device_owner=stepler_config.PORT_DEVICE_OWNER_SERVER,
+        device_id=server.id)
+    floating_ip = create_floating_ip(public_network, port=server_port)
+
+    # Check that DHCP server returns NTP server IP
+    with server_steps.get_server_ssh(
+            server, ip=floating_ip['floating_ip_address']) as server_ssh:
+        with server_ssh.sudo():
+            server_ssh.check_call('apt-get install -yq nmap')
+            stdout = server_ssh.check_call(
+                'nmap --script broadcast-dhcp-discover').stdout
+    assert_that(stdout, contains_string('Time Server: ' + ntp_ip))
