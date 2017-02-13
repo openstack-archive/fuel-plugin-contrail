8d17d7ca0a495d8c90948a28837a150a342dc62a
Georgy Dyuldin <g.dyuldin@gmail.com>
2017-02-13 15:53:32 +0300

    Add test_cidr_for_source_and_destination
    Change-Id: I1ba16dbdaa023b982343a28f2130aa825b4586c9

--- plugin_test/vapor/vapor/tests/test_policy.py
+++ plugin_test/vapor/vapor/tests/test_policy.py
@@ -106,3 +106,72 @@ def test_policy_with_local_source(
     for server in servers[1:]:
         with server_steps.get_server_ssh(server) as server_ssh:
             connectivity.check_icmp_connection_status(ip, remote=server_ssh)
+
+
+def test_cidr_for_source_and_destination(
+        public_network, cirros_image, flavor, security_group, network, subnet,
+        contrail_network_policy, set_network_policy, create_floating_ip,
+        server_steps, port_steps, contrail_api_client):
+    """Test CIDR as match criteria for source and destination.
+
+    Steps:
+        #. Create network
+        #. Create 3 servers
+        #. Create policy with deny ICMP traffic from server1 ip to server2 ip
+            (with /32 CIDR)
+        #. Ping between vm1 and vm2. Ping should fail
+        #. Ping between vm1 and vm3. Ping should pass
+    """
+    # Boot servers
+    server1, server2, server3 = server_steps.create_servers(
+        count=3,
+        image=cirros_image,
+        flavor=flavor,
+        networks=[network],
+        security_groups=[security_group],
+        username=stepler_config.CIRROS_USERNAME,
+        password=stepler_config.CIRROS_PASSWORD)
+
+    # Add floating IP to server1
+    port = port_steps.get_port(
+        device_owner=stepler_config.PORT_DEVICE_OWNER_SERVER,
+        device_id=server1.id)
+    floating_ip = create_floating_ip(public_network, port=port)
+    server_steps.check_server_ip(
+        server1,
+        floating_ip['floating_ip_address'],
+        timeout=settings.FLOATING_IP_BIND_TIMEOUT)
+
+    # Block ICMP traffic from server1 to server2
+    ip1 = server_steps.get_fixed_ip(server1)
+    ip2 = server_steps.get_fixed_ip(server2)
+    ip3 = server_steps.get_fixed_ip(server3)
+    rule = types.PolicyRuleType(
+        protocol='icmp',
+        direction='<>',
+        src_addresses=[
+            types.AddressType(subnet=types.SubnetType(
+                ip_prefix=ip1, ip_prefix_len=32))
+        ],
+        src_ports=[types.PortType()],
+        dst_addresses=[
+            types.AddressType(subnet=types.SubnetType(
+                ip_prefix=ip2, ip_prefix_len=32))
+        ],
+        dst_ports=[types.PortType()],
+        action_list=types.ActionListType(simple_action='deny'))
+    contrail_network_policy.set_network_policy_entries(
+        types.PolicyEntriesType(policy_rule=[rule]))
+    contrail_api_client.network_policy_update(contrail_network_policy)
+
+    # Bind policy to network
+    contrail_network = contrail_api_client.virtual_network_read(
+        id=network['id'])
+    set_network_policy(contrail_network, contrail_network_policy)
+
+    # Check pings
+    with server_steps.get_server_ssh(
+            server1, floating_ip['floating_ip_address']) as server_ssh:
+        connectivity.check_icmp_connection_status(
+            ip2, remote=server_ssh, must_available=False)
+        connectivity.check_icmp_connection_status(ip3, remote=server_ssh)
