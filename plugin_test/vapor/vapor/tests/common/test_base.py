3bf949db9cef34c54930854496f704440a7803ba
Georgy Dyuldin <g.dyuldin@gmail.com>
2017-02-27 11:31:33 +0300

    Add test_create_multiple_servers__on_many_networks
    Change-Id: I282933608d78bb0efff083aca90cd9bc2ec8f29e

--- plugin_test/vapor/vapor/tests/common/test_base.py
+++ plugin_test/vapor/vapor/tests/common/test_base.py
@@ -379,3 +379,25 @@ def test_create_server_on_network_without_subnet(
             }],
             check=False),
         raises(nova_exceptions.BadRequest, 'requires a subnet'))
+
+
+@pytest.mark.parametrize('flavor', [dict(ram=128, disk=1)], indirect=True)
+def test_create_multiple_servers__on_many_networks(
+        cirros_image, flavor, create_network, create_subnet, server_steps):
+    """Validate creation of multiple VN with multiple subnet and VMs in it.
+
+    Steps:
+        #. Create 2 networks with subnets
+        #. Create 4 servers on each of network
+        #. Check that all servers reach ACTIVE state
+    """
+    networks = []
+    for name in utils.generate_ids(count=2):
+        network = create_network(name)
+        networks.append(network)
+        create_subnet(
+            name + '__subnet', network=network, cidr=stepler_config.LOCAL_CIDR)
+
+    for network in networks:
+        server_steps.create_servers(
+            count=4, flavor=flavor, image=cirros_image, networks=[network])
