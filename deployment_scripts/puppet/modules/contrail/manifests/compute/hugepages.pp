0a6540143ab468db6fb9ec68ff89c9325d58f031
Vlad Naboichenko <vnaboychenko@mirantis.com>
2017-03-24 10:19:14 -0700

    Refactor logic for dpdk computes
    [DE271525]
    
    Change-Id: I3bd995d87123d2f5bc72d4e6e77518481a14909d

--- deployment_scripts/puppet/modules/contrail/manifests/compute/hugepages.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/compute/hugepages.pp
@@ -18,34 +18,10 @@ class contrail::compute::hugepages {
   $node_hash       = hiera_hash('node', {})
   $nova_huge_pages = pick($node_hash['nova_hugepages_enabled'], false)
 
-  if $contrail::compute_dpdk_enabled and !$nova_huge_pages and ($::osfamily == 'Debian') {
-    $qemu_hugepages_value    = 'set KVM_HUGEPAGES 1'
-    $libvirt_hugetlbfs_mount = 'set hugetlbfs_mount /run/hugepages/kvm'
-    augeas { 'qemu_hugepages':
-      context => '/files/etc/default/qemu-kvm',
-      changes => $qemu_hugepages_value,
-      notify  => Service[$contrail::libvirt_name],
-    }
-    augeas { 'libvirt_hugetlbfs_mount':
-      context => '/files/etc/libvirt/qemu.conf',
-      changes => $libvirt_hugetlbfs_mount,
-      notify  => Service[$contrail::libvirt_name],
-    }
-    service { 'qemu-kvm':
-      ensure => running,
-      enable => true,
-    }
-    service { $contrail::libvirt_name:
-      ensure => running,
-      enable => true,
-    }
-
+  if $nova_huge_pages and ($::osfamily == 'Debian') {
     kernel_parameter { 'transparent_hugepage':
       ensure => present,
       value  => 'never',
     }
-
-    Augeas['qemu_hugepages'] ~> Service<| title == 'qemu-kvm'|>
-    Service<| title == 'qemu-kvm'|> -> Service<| title == $contrail::libvirt_name |>
   }
 }
