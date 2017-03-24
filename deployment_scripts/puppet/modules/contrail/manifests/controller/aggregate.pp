8194d23fa616b3e06d2cde6540e0976291e5b028
Vlad Naboichenko <vnaboychenko@mirantis.com>
2017-03-24 10:28:57 -0700

    Increase memory size for m1.small.hpgs to 1024
    [DE273154]
    
    Change-Id: Ie91be4cf95771ce5ec9fef21dc2027885a42f701

--- deployment_scripts/puppet/modules/contrail/manifests/controller/aggregate.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/controller/aggregate.pp
@@ -58,7 +58,7 @@ class contrail::controller::aggregate {
 
 # Create flavor for huge pages
     exec { 'create-m1.small.hpgs-flavor' :
-      command => 'bash -c "nova flavor-create --is-public true m1.small.hpgs auto 512 20 2"',
+      command => 'bash -c "nova flavor-create --is-public true m1.small.hpgs auto 1024 20 2"',
       unless  => 'bash -c "nova flavor-list | grep -q m1.small.hpgs"',
     } ->
     exec { 'create-m1.small.hpgs-mempage' :
