24e6d0727c22ea96ded9c6a948a6931864465dd2
Hemachandra Reddy <hr858f@att.com>
2017-04-21 12:10:41 -0500

    As per https://review.opencontrail.org/#/c/19149/, sandesh_send_rate_limit needs to be enabled in all those config files that Juniper made this option available. Also, Juniper recommended the value to be set to 10. They also made it default here: https://review.opencontrail.org/#/c/24421/
    Change-Id: I099c37dfcb48b621ae83591a382234ce3906f591

--- deployment_scripts/puppet/modules/contrail/manifests/init.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/init.pp
@@ -296,7 +296,7 @@ class contrail {
   $aaa_mode = pick($settings['aaa_mode'], 'cloud-admin')
 
   #Sandesh settings
-  $sandesh_send_rate_limit = pick($settings['sandesh_send_rate_limit'], '100')
+  $sandesh_send_rate_limit = pick($settings['sandesh_send_rate_limit'], '10')
 
   $analytics_config_audit_ttl = pick($settings['analytics_config_audit_ttl'], '2160')
   $analytics_statistics_ttl   = pick($settings['analytics_statistics_ttl'], '24')
