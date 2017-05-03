0caa6864c9c9c2b6ded475e03523b109f6b9d6ae
Vlad Naboichenko <vnaboychenko@mirantis.com>
2017-05-02 17:07:34 -0700

    Add pins for libvirt0 and qemu-kvm package
    Force pins qemu and libvirt to install from contrail repos for
    kernel vrouter
    
    Change-Id: I17cdba116dcc7aef75ca2b7143413934508a7b6a

--- deployment_scripts/puppet/modules/contrail/manifests/compute/compute_override.pp
+++ deployment_scripts/puppet/modules/contrail/manifests/compute/compute_override.pp
@@ -31,60 +31,63 @@ class contrail::compute::compute_override {
     packages    => $common_pkg,
   }
 
-  if $contrail::compute_dpdk_enabled {
-    # Create local dpdk repository
-    package { 'dpdk-depends-packages':
-      ensure => present,
-    } ->
-    exec { 'update-dpdk-repo':
-      command => 'dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz',
-      cwd     => '/opt/contrail/contrail_install_repo_dpdk',
-    } ->
-    file {'/opt/contrail/contrail_install_repo_dpdk/Release':
-      ensure  => file,
-      content => 'Label: dpdk-depends-packages',
-    } ->
-    apt::source { 'dpdk-depends-repo':
-      location    => 'file:/opt/contrail/contrail_install_repo_dpdk',
-      repos       => './',
-      release     => ' ',
-      include_src => false,
-    }
-
-    # Patch nova packages if it set on Fuel UI
-    if $contrail::patch_nova {
-      apt::pin { 'contrail-pin-nova':
-        explanation => 'Prevent patched python-nova from upgrades',
-        priority    => 1200,
-        label       => '1:2015.1.1-1~u14.04+mos19665',
-        packages    => 'python-nova',
-      } ->
-      file { "${patch_path}/nova-dpdk-vrouter.patch":
-        ensure => present,
-        mode   => '0644',
-        source => 'puppet:///modules/contrail/nova-dpdk-vrouter.patch',
-      }->
-      exec { 'patch-python-nova':
-        command => 'patch -p1 < nova-dpdk-vrouter.patch && touch python-nova-patch-DONE',
-        cwd     => $patch_path,
-        creates => "${patch_path}/python-nova-patch-DONE",
-      }
-    }
-
     # Override libvirt and qemu packages if it set on Fuel UI
     if $contrail::install_contrail_qemu_lv {
+      if $contrail::compute_dpdk_enabled {
+        $pin_label = 'dpdk-depends-packages'
+
+        package { 'dpdk-depends-packages':
+          ensure => present,
+        } ->
+        exec { 'update-dpdk-repo':
+          command => 'dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz',
+          cwd     => '/opt/contrail/contrail_install_repo_dpdk',
+        } ->
+        file {'/opt/contrail/contrail_install_repo_dpdk/Release':
+          ensure  => file,
+          content => "Label: ${pin_label}",
+        } ->
+        apt::source { 'dpdk-depends-repo':
+          location    => 'file:/opt/contrail/contrail_install_repo_dpdk',
+          repos       => './',
+          release     => ' ',
+          include_src => false,
+          before      => Package['qemu-kvm', 'libvirt0', 'libvirt-bin']
+        } ->
+        apt::pin { 'contrail-pin-qemu':
+          explanation => 'Install qemu from dpdk-depends',
+          priority    => 1200,
+          label       => $pin_label,
+          packages    => ['qemu', 'qemu-*'],
+          before      => Package[$qemu_pkg],
+        }
+
+      } else {
+          $pin_label = 'contrail'
+        }
+
       # For qemu you don't need additional pinning, only dpdk repository
-      apt::pin { 'contrail-pin-qemu':
-        explanation => 'Install qemu from dpdk-depends',
-        priority    => 1200,
-        label       => 'dpdk-depends-packages',
-        packages    => ['qemu', 'qemu-*'],
-      } ->
-      package{$qemu_pkg:
-        ensure          => latest,
-        provider        => apt,
-        install_options => $install_options,
+      if !defined(Package['qemu-kvm']) {
+        package{'qemu-kvm':
+          ensure          => latest,
+          provider        => apt,
+          install_options => $install_options,
+        }
       }
+      if !defined(Package['qemu-system-x86']) {
+        package{'qemu-system-x86':
+          ensure          => latest,
+          provider        => apt,
+          install_options => $install_options,
+        }
+      }
+      if !defined(Package['qemu-system-common']) {
+        package{'qemu-system-common':
+          ensure          => latest,
+          provider        => apt,
+          install_options => $install_options,
+        }
+       }
       if !defined(Service['qemu-kvm']) {
         service { 'qemu-kvm':
           ensure  => running,
@@ -96,13 +99,24 @@ class contrail::compute::compute_override {
         explanation => 'Set priority for packages that need to override from contrail repository',
         packages    => $libvirt_pkg,
         priority    => 1200,
-        label       => 'dpdk-depends-packages',
+        label       => $pin_label,
         require     => Service['qemu-kvm'],
-      } ->
-      package{ $libvirt_pkg:
-        ensure          => latest,
-        provider        => apt,
-        install_options => $install_options,
+      }
+      if !defined(Package['libvirt-bin']) {
+        package{'libvirt-bin':
+          ensure          => latest,
+          provider        => apt,
+          install_options => $install_options,
+          require         => Apt::Pin['contrail-override-libvirt'],
+        }
+      }
+      if !defined(Package['libvirt0']) {
+        package{'libvirt0':
+          ensure          => latest,
+          provider        => apt,
+          install_options => $install_options,
+          require         => Apt::Pin['contrail-override-libvirt'],
+        }
       }
       # With a new libvirt packages this init script must be stopped
       if !defined(Service['libvirtd']) {
@@ -118,6 +132,8 @@ class contrail::compute::compute_override {
           enable => false,
         }
       }
+    }
+
       # This options must be uncommented for correct work with nova-compute
       file_line { 'add_libvirt_opt1':
         path    => '/etc/libvirt/libvirtd.conf',
@@ -141,6 +157,26 @@ class contrail::compute::compute_override {
           require => Service[$contrail::libvirt_name],
         }
       }
+
+    # Patch nova packages if it set on Fuel UI
+    if $contrail::patch_nova {
+      apt::pin { 'contrail-pin-nova':
+        explanation => 'Prevent patched python-nova from upgrades',
+        priority    => 1200,
+        label       => '1:2015.1.1-1~u14.04+mos19665',
+        packages    => 'python-nova',
+      } ->
+      file { "${patch_path}/nova-dpdk-vrouter.patch":
+        ensure => present,
+        mode   => '0644',
+        source => 'puppet:///modules/contrail/nova-dpdk-vrouter.patch',
+      }->
+      exec { 'patch-python-nova':
+        command => 'patch -p1 < nova-dpdk-vrouter.patch && touch python-nova-patch-DONE',
+        onlyif  => 'patch --dry-run -p1 < nova-dpdk-vrouter.patch',
+        cwd     => $patch_path,
+        creates => "${patch_path}/python-nova-patch-DONE",
+      }
     }
-  }
 }
+
